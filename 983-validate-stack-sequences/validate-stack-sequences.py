21c2110312e604a04aaf1643311aaa07074bf856
nitishraghunathan <nitish.raghunathan1994@gmail.com>
2023-09-13 05:43:49 -0700

    Time: 74 ms (57.40%) | Memory: 16.5 MB (69.97%) - LeetSync

--- 983-validate-stack-sequences/validate-stack-sequences.py
+++ 983-validate-stack-sequences/validate-stack-sequences.py
@@ -1,23 +1,20 @@
 class Solution:
     def validateStackSequences(self, pushed: List[int], popped: List[int]) -> bool:
         """
-        we need to construct a sequence from pushed to form popped 
-        1. We can iterate pushed array and check if we find the first matching element in popped array.
-        2. then we increase counter in popped array and push elements till we find the next element.
-        3. If we hit the end of the list we stop.
-        4. we pop all the elements from the stack and compare with popped.
-        5. If it is the same we will not find any issues.
-        6. else we return false
+        We need to construct a sequence from pushed to form popped
+        1. We can iterate pushed array and check if we find the first matching element in the pushed array.
+        2. Increase the counter in popped array and push elements till will find the next element which is the same
+        3. If we hit the end of the list we stop
+        4. We pop the elements in the stack and check with popped array if the sequence is formed
+        5. We return our result based on the comparison
         """
-        if not popped or not pushed or len(pushed)!= len(popped):
+        if not popped or not pushed or len(pushed) != len(popped):
             return False
-        
         stack = []
         counter = 0
         for i in range(len(pushed)):
             stack.append(pushed[i])
-            while stack and stack[-1] ==  popped[counter]:
-                    counter+=1
-                    stack.pop()
-
+            while stack and stack[-1] == popped[counter]:
+                counter +=1
+                stack.pop()
         return len(stack) == 0
