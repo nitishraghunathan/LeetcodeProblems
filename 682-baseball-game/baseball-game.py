a989413d71c4c48b4808aebd9f916b97297fefd4
nitishraghunathan <nitish.raghunathan1994@gmail.com>
2023-09-14 05:40:54 -0700

    Time: 49 ms (60.64%) | Memory: 16.3 MB (93.06%) - LeetSync

--- 682-baseball-game/baseball-game.py
+++ 682-baseball-game/baseball-game.py
@@ -1,14 +1,19 @@
-class Solution(object):
-    def calPoints(self, ops):
+class Solution:
+    def calPoints(self, operations: List[str]) -> int:
+        """
+        1. Add all operands to the stack 
+        2. Pop elements according the operator or capital character
+        3. Perfrom special operations
+        4. sum all elements in the stack
+        """
         stack = []
-        for op in ops:
+        for op in operations:
             if op == '+':
                 stack.append(stack[-1] + stack[-2])
             elif op == 'C':
                 stack.pop()
             elif op == 'D':
-                stack.append(2 * stack[-1])
+                stack.append(stack[-1]*2)
             else:
                 stack.append(int(op))
-
         return sum(stack)
