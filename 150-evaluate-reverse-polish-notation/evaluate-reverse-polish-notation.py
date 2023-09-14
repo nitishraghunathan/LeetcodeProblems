b780b982e982a8b079e2c3d9f541d1783e7c9b82
nitishraghunathan <nitish.raghunathan1994@gmail.com>
2023-09-13 19:47:07 -0700

    Time: 74 ms (60.30%) | Memory: 16.7 MB (49.28%) - LeetSync

--- 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.py
+++ 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.py
@@ -1,18 +1,23 @@
 class Solution:
     def evalRPN(self, tokens: List[str]) -> int:
+        """
+        1. Add all the strings to the stack
+        2. When there is an operator pop two stack elements and perfrom operations and append them back to the slack
+        3. If there are no operators it is assumed to be addition
+        """
         polish_stack = []
-        
+
         for expression in tokens:
             if expression == '+' or expression == '-' or expression == '*' or expression == '/':
                 polish_sum = 0
-                first_num  = polish_stack.pop()
+                first_num = polish_stack.pop()
                 second_num = polish_stack.pop()
                 if expression == '+':
                     polish_sum = second_num + first_num
                 if expression == '-':
-                    polish_sum =  second_num - first_num
+                    polish_sum = second_num - first_num
                 if expression == '*':
-                     polish_sum =  second_num* first_num
+                    polish_sum = second_num * first_num
                 if expression == '/':
                     polish_sum = int(second_num/first_num)
                 polish_stack.append(polish_sum)
