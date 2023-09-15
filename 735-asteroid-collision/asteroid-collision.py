32f0e3876b3037738288113f43213f39174e67c1
nitishraghunathan <nitish.raghunathan1994@gmail.com>
2023-09-15 06:52:58 -0700

    Time: 108 ms (53.70%) | Memory: 17.3 MB (93.88%) - LeetSync

--- 735-asteroid-collision/asteroid-collision.py
+++ 735-asteroid-collision/asteroid-collision.py
@@ -1,30 +1,33 @@
-# class Solution:
-#     def asteroidCollision(self, asteroids: List[int]) -> List[int]:
-#         while len(asteroids) > 1 and ((asteroids[-1] < 0 and asteroids[-2] > 0) or (asteroids[-1] > 0 and asteroids[-2] <0)):
-#             first = asteroids.pop()
-#             second = asteroids.pop()
-#             abs_first = abs(first)
-#             abs_second = abs(second)
-#             if abs_first == abs_second:
-#                 continue
-#             elif abs_first > abs_second:
-#                 asteroids.append(first)
-#             else:
-#                 asteroids.append(second)
-            
-#         return asteroids
-
 class Solution:
     def asteroidCollision(self, asteroids: List[int]) -> List[int]:
-        st=[]
-        for n in asteroids:
-            st.append(n)
-            while len(st)>1 and ((st[-2]>0) and (st[-1]<0)):
-                m,n = st.pop(),st.pop()
-                if abs(m)!=abs(n):
-                    if abs(m)>abs(n):
-                        st.append(m)
-                    else:
-                        st.append(n)
+        stack = []
+        for asteroid in asteroids:
+            stack.append(asteroid)
+            while len(stack) > 1 and (stack[-1] < 0 and stack[-2] > 0):
+                first = stack.pop()
+                second = stack.pop()
+                abs_first = abs(first)
+                abs_second = abs(second)
+                if abs_first == abs_second:
+                    continue
+                elif abs_first > abs_second:
+                    stack.append(first)
+                else:
+                    stack.append(second)
+            
+        return stack
+
+# class Solution:
+#     def asteroidCollision(self, asteroids: List[int]) -> List[int]:
+#         st=[]
+#         for n in asteroids:
+#             st.append(n)
+#             while len(st)>1 and ((st[-2]>0) and (st[-1]<0)):
+#                 m,n = st.pop(),st.pop()
+#                 if abs(m)!=abs(n):
+#                     if abs(m)>abs(n):
+#                         st.append(m)
+#                     else:
+#                         st.append(n)
         
-        return st
+#         return st
