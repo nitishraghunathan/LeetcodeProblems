d41937e0d9e36ad2dbb863426da2e09c45953c56
nitishraghunathan <nitish.raghunathan1994@gmail.com>
2023-09-27 20:15:07 -0700

    Time: 760 ms (58.41%) | Memory: 38.2 MB (65.03%) - LeetSync

--- 883-car-fleet/car-fleet.py
+++ 883-car-fleet/car-fleet.py
@@ -1,12 +1,15 @@
 class Solution:
     def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
-        new_list = [(pos, vel) for pos, vel in zip(position, speed)]
+        """
+        1. Sort all the vehicles by position and speed
+        2. Add them to a stack ande check if time taken is less than the other car if yes then a fleet is not formed and we pop the time out. If not we continue forming a new fleet
+        3. we return the length of the stack
+        """
+        new_list = [(pos,vel) for pos, vel in zip(position, speed)]
         new_list.sort(reverse=True)
-        stack=[]
+        stack = []
         for pos, vel in new_list:
             stack.append((target-pos)/vel)
-            if len(stack) > 1 and stack[-2] >= stack[-1]:
+            if len(stack) > 1 and stack[-1] <= stack[-2]:
                 stack.pop()
         return len(stack)
-
-            
