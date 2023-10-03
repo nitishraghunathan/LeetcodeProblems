981ac4a4a88443931ce2ac1046d0310ba022a016
nitishraghunathan <nitish.raghunathan1994@gmail.com>
2023-10-03 15:21:55 -0700

    Time: 87 ms (81.94%) | Memory: 20.9 MB (66.09%) - LeetSync

--- 1320-remove-all-adjacent-duplicates-in-string-ii/remove-all-adjacent-duplicates-in-string-ii.py
+++ 1320-remove-all-adjacent-duplicates-in-string-ii/remove-all-adjacent-duplicates-in-string-ii.py
@@ -1,13 +1,19 @@
 class Solution:
-    def removeDuplicates(self, s: str, k: int) -> str:        
-        stck = []    
-        
-        for c in s:                            
-            if stck and stck[-1][0] == c: # check if stack is not empty
-                stck[-1][1]+=1
-                if stck[-1][1] == k:
-                    stck.pop()
+    def removeDuplicates(self, s: str, k: int) -> str:
+        """
+        1. Need to deduplicate k instance fo an alphabet and rremove them
+        2. We store all the alphabets with their count in  a stack
+        3. We check the lats elemnt of the stack while iteration to see if the alphabet is the same, if same we increase the count if not we add the elemtn with count 1
+        4. Once the count reaches K we pop the element from the stack
+        5. we join by concatenating the elements in the stack
+        """
+        stack = []
+        for character in s:
+            if stack and stack[-1][0] == character:
+                stack[-1][1] +=1
+                if stack[-1][1] == k:
+                    stack.pop()
             else:
-                stck.append([c, 1])            
+                stack.append([character, 1])
+        return "".join([count*character for character, count in stack])
         
-        return ''.join(c * cnt for c, cnt in stck)   
