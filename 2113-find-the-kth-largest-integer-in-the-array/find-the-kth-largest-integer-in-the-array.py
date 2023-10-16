a2baa2e299904f4e60d7808bd6c1291cae977a29
nitishraghunathan <nitish.raghunathan1994@gmail.com>
2023-10-15 21:59:01 -0700

    Time: 277 ms (50.51%) | Memory: 25 MB (77.50%) - LeetSync

--- 2113-find-the-kth-largest-integer-in-the-array/find-the-kth-largest-integer-in-the-array.py
+++ 2113-find-the-kth-largest-integer-in-the-array/find-the-kth-largest-integer-in-the-array.py
@@ -1,8 +1,14 @@
 class Solution:
     def kthLargestNumber(self, nums: List[str], k: int) -> str:
+        """
+        1. heap queue is intiutive to this problem.
+        2. We add elements to the queue and we pop them if the size > k
+        3. return the head of the queue
+        """
         queue = []
         for num in nums:
             heapq.heappush(queue, int(num))
             if len(queue) > k:
                 heapq.heappop(queue)
         return str(queue[0])
+        
