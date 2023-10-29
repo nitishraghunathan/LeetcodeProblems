2dea16bf3e4a3b4cdb5b0ea5120989f667ce686b
nitishraghunathan <nitish.raghunathan1994@gmail.com>
2023-10-29 16:19:10 -0700

    Time: 140 ms (79.84%) | Memory: 18 MB (20.63%) - LeetSync

--- 283-move-zeroes/move-zeroes.py
+++ 283-move-zeroes/move-zeroes.py
@@ -2,12 +2,15 @@ class Solution:
     def moveZeroes(self, nums: List[int]) -> None:
         """
         Do not return anything, modify nums in-place instead.
+        1. Take a pointer and keep track if element is zero is element is non zero replace the element at the pointers position.
+        2. replace the remaining elements from 1st non zero position (pointer's position) with 0
         """
         count = 0
         for index, value in enumerate(nums):
             if value !=0:
                 nums[count]=value
-                count+=1
+                count +=1
         for i in range(count, len(nums)):
-            nums[i]=0
+            nums[i] = 0
         return nums
+        
