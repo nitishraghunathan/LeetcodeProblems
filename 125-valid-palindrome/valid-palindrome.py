a7eed9686a1a6ba2c8c38685d5d64105be1626aa
nitishraghunathan <nitish.raghunathan1994@gmail.com>
2023-10-29 15:49:11 -0700

    Time: 54 ms (38.44%) | Memory: 16.9 MB (91.29%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.py
+++ 125-valid-palindrome/valid-palindrome.py
@@ -1,18 +1,24 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
+        """
+         1. check if the string reads the same forward and backward.
+         2. we need to check only alphanumeric characters
+         3. we also need to check if they are lowercase if not we convert them to lowercase
+         4. Its more efficient to use two pointers
+         5. we keep iterating the start pointer and the end pointer till we intersect.
+         6. If we find an anomaly we return false else we conclude it is a palindrome
+        """
         start = 0
         end = len(s)-1
         while start < end:
             if not s[start].isalnum():
-                start +=1
+                start += 1
             elif not s[end].isalnum():
-                end -=1
+                    end -= 1
             else:
                 if s[start].lower() != s[end].lower():
                     return False
-                start +=1
-                end -=1
-        
+                start += 1
+                end -= 1
         return True
-        
         
