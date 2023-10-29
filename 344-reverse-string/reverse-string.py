38aa65b32ceda7376557076b8debefd007f1445c
nitishraghunathan <nitish.raghunathan1994@gmail.com>
2023-10-29 16:05:49 -0700

    Time: 201 ms (19.85%) | Memory: 20.7 MB (63.06%) - LeetSync

--- 344-reverse-string/reverse-string.py
+++ 344-reverse-string/reverse-string.py
@@ -2,11 +2,15 @@ class Solution:
     def reverseString(self, s: List[str]) -> None:
         """
         Do not return anything, modify s in-place instead.
+        1. we use two pointers start and end
+        2. swap start position with end position characters
+        3. return the string
         """
         start = 0
-        end = len(s)-1
+        end = len(s) -1 
         while start < end:
             s[start], s[end] = s[end], s[start]
-            start +=1
-            end-=1
+            start += 1
+            end -= 1
         return s
+        
