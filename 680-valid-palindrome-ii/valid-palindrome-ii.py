b068a3f77b14b00c3e8e434d68332153ac6f71f5
nitishraghunathan <nitish.raghunathan1994@gmail.com>
2023-10-29 16:01:12 -0700

    Time: 130 ms (46.42%) | Memory: 16.8 MB (52.01%) - LeetSync

--- 680-valid-palindrome-ii/valid-palindrome-ii.py
+++ 680-valid-palindrome-ii/valid-palindrome-ii.py
@@ -1,21 +1,26 @@
 class Solution:
     def validPalindrome(self, s: str) -> bool:
+        """
+        1. we get two occurences of all alphabets
+        2. we get one occurence of only one alphabet buth two occurences of all other alphabets
+        3. We would use the two pointer method 
+        """
         def check_palindrome(s, i, j):
             while i < j:
                 if s[i] != s[j]:
                     return False
                 i += 1
                 j -= 1
-            
-            return True
 
+            return True
         i = 0
-        j = len(s) - 1
+        j = len(s) -1
         while i < j:
-            # Found a mismatched pair - try both deletions
             if s[i] != s[j]:
-                return check_palindrome(s, i, j - 1) or check_palindrome(s, i + 1, j)
+                return check_palindrome(s, i, j-1) or check_palindrome(s, i + 1, j)
+            
             i += 1
             j -= 1
-        
         return True
+
+        
