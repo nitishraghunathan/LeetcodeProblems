a3bdb69aa184942bc170c9361a470dc9c7b4aea2
nitishraghunathan <nitish.raghunathan1994@gmail.com>
2023-11-14 06:16:57 -0800

    Time: 819 ms (55.85%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1462-list-the-products-ordered-in-a-period/list-the-products-ordered-in-a-period.sql
@@ -0,0 +1,2 @@
+/* Write your T-SQL query statement below */
+SELECT * FROM (SELECT Products.product_name, SUM(Orders.unit) AS unit FROM Products JOIN Orders ON Products.product_id=Orders.product_id  WHERE Orders.order_date BETWEEN '2020-02-01' AND '2020-02-29' GROUP BY Products.product_name) AS A WHERE A.unit >=100
