cace3359c9cba7e81549c8efd0333e2b4bb6436f
nitishraghunathan <nitish.raghunathan1994@gmail.com>
2024-01-25 05:34:54 -0800

    Time: 39 ms (87.13%) | Memory: 16.6 MB (63.97%) - LeetSync

--- 1058-lexicographically-smallest-equivalent-string/lexicographically-smallest-equivalent-string.py
+++ 1058-lexicographically-smallest-equivalent-string/lexicographically-smallest-equivalent-string.py
@@ -1,3 +1,12 @@
+"""
+we need to find potential ways.
+
+1. a graph can be modelled for this problem 
+Brute force -> compare each character and find the smallest mapping.
+We can use union Find.
+Time complexity -> O(N) for find 
+O(N) -> for union 
+"""
 class UnionFind:
     def __init__(self):
         self.root = [i for i in range(26)]
@@ -8,7 +17,7 @@ class UnionFind:
             return x
         self.root[x] = self.find(self.root[x])
         return self.root[x]
-    def union(self, x,y):
+    def union(self, x, y):
         rootx = self.find(x)
         rooty = self.find(y)
         if rootx != rooty:
@@ -16,18 +25,17 @@ class UnionFind:
                 for index,value in enumerate(self.root):
                     if value == rooty:
                         self.root[index] = self.root[rootx]
-            elif self.rank[rooty] > self.rank[rootx]:
+            else:
                 for index,value in enumerate(self.root):
                     if value == rootx:
-                        self.root[index] = self.root[rooty]  
+                        self.root[index] = self.root[rooty]
     def is_connected(self, x, y):
         return self.find(x) == self.find(y)
-
 class Solution:
     def smallestEquivalentString(self, s1: str, s2: str, baseStr: str) -> str:
         uf = UnionFind()
         for one, two in zip(s1, s2):
-            uf.union(ord(one)-97, ord(two)-97)
+            uf.union(ord(one)-97, ord(two) -97)
         new_str = ''
         for characters in baseStr:
             new_str += chr(uf.root[ord(characters)-97] + 97)
