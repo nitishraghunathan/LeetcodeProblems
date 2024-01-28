416a362f7ee811c5e4400035af9736578ac76ffe
nitishraghunathan <nitish.raghunathan1994@gmail.com>
2024-01-27 18:37:37 -0800

    Time: 652 ms (14.71%) | Memory: 23.8 MB (27.43%) - LeetSync

---
+++ 1100-connecting-cities-with-minimum-cost/connecting-cities-with-minimum-cost.py
@@ -0,0 +1,29 @@
+class Solution:
+    def minimumCost(self, n: int, connections: List[List[int]]) -> int:
+        result = []
+      
+        map_dict = {}
+        visited = set()
+        for connection in connections:
+            if connection[0] not in map_dict:
+                map_dict[connection[0]] = []
+            if connection[1] not in map_dict:
+                map_dict[connection[1]] = []
+            map_dict[connection[0]].append([connection[2], connection[1]])
+            map_dict[connection[1]].append([connection[2], connection[0]])
+
+        total_cost = 0
+        for key in map_dict[1]:
+            heapq.heappush(result, key)
+        heapq.heapify(result)
+        visited.add(1)
+        while result:
+            array = heapq.heappop(result)
+            print(array)
+            if array[1] not in visited:
+                visited.add(array[1])
+                total_cost += array[0]
+                for key in map_dict[array[1]]:
+                    heapq.heappush(result, key)
+
+        return total_cost if len(visited) == n else -1
