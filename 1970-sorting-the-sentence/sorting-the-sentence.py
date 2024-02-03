5ac7cc748b4c843cae06173baa1696a5ee659cde
nitishraghunathan <nitish.raghunathan1994@gmail.com>
2024-02-02 19:26:16 -0800

    Time: 38 ms (50.74%) | Memory: 16.4 MB (85.08%) - LeetSync

--- 1970-sorting-the-sentence/sorting-the-sentence.py
+++ 1970-sorting-the-sentence/sorting-the-sentence.py
@@ -3,6 +3,5 @@ class Solution:
         words = s.split(' ')
         result = ['']*len(words)
         for word in words:
-            print(word[len(word)-1])
             result[int(word[len(word)-1])-1] = word[:len(word)-1] + ' '
         return ''.join(result).strip()
