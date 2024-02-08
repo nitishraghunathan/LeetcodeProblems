e498936b083c63300469c1415932449a6693b46c
nitishraghunathan <nitish.raghunathan1994@gmail.com>
2024-02-08 05:59:32 -0800

    Time: 45 ms (80.65%) | Memory: 18.2 MB (83.29%) - LeetSync

--- 530-minimum-absolute-difference-in-bst/minimum-absolute-difference-in-bst.py
+++ 530-minimum-absolute-difference-in-bst/minimum-absolute-difference-in-bst.py
@@ -4,21 +4,14 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
-class Solution:
-    def getMinimumDifference(self, root: Optional[TreeNode]) -> int:
-        min_diff= float('inf')
-        def dfs(root: Optional[TreeNode], result: list):
-            if root is None:
-                return 
-            nonlocal min_diff
-            dfs(root.left, result)
-            result.append(root.val)
-            if len(result) > 1:
-                min_diff = min(min_diff, result[len(result)-1]-result[len(result)-2])
-            dfs(root.right, result)
-            return 
-        dfs(root, [])
-        return min_diff
+class Solution(object):
+    def getMinimumDifference(self, root):
+        def fn(node, lo, hi):
+            if not node: return hi - lo
+            left = fn(node.left, lo, node.val)
+            right = fn(node.right, node.val, hi)
+            return min(left, right)
+        return fn(root, float('-inf'), float('inf'))
 
 
 
