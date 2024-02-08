6d533aa0587800058c9d1ea6a0b62601ace3bd3f
nitishraghunathan <nitish.raghunathan1994@gmail.com>
2024-02-08 05:47:56 -0800

    Time: 40 ms (65.83%) | Memory: 16.6 MB (72.00%) - LeetSync

---
+++ 2316-count-hills-and-valleys-in-an-array/count-hills-and-valleys-in-an-array.py
@@ -0,0 +1,11 @@
+class Solution:
+    def countHillValley(self, nums: List[int]) -> int:
+        # remove adjacent duplicates but preserve order
+        nums = [item[0] for item in itertools.groupby(nums)]
+    
+        count = 0
+        for i in range(1, len(nums)-1):
+            if (nums[i] > nums[i-1] and nums[i] > nums[i+1]) or (nums[i] < nums[i-1] and nums[i] < nums[i+1]):    
+                count += 1
+        return count  
+
