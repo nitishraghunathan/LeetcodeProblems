edfd0a951fd1a49a1679514c8cf2cbfc734de917
nitishraghunathan <nitish.raghunathan1994@gmail.com>
2024-02-15 22:49:22 -0800

    Time: 1529 ms (5.06%) | Memory: 34.1 MB (49.87%) - LeetSync

--- 1909-buildings-with-an-ocean-view/buildings-with-an-ocean-view.py
+++ 1909-buildings-with-an-ocean-view/buildings-with-an-ocean-view.py
@@ -4,6 +4,6 @@ class Solution:
         max_value = float('-inf')
         for i in range(len(heights)-1, -1, -1):
             if heights[i] > max_value:
-                result.append(i)
+                result.insert(0,i)
                 max_value = heights[i]
-        return sorted(result)
+        return result
