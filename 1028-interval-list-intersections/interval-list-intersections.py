eae398ff2e65a35c9acb071b989ac59a46b47e19
nitishraghunathan <nitish.raghunathan1994@gmail.com>
2024-02-21 06:52:30 -0800

    Time: 145 ms (9.76%) | Memory: 17.7 MB (39.43%) - LeetSync

--- 1028-interval-list-intersections/interval-list-intersections.py
+++ 1028-interval-list-intersections/interval-list-intersections.py
@@ -7,8 +7,6 @@ class Solution:
         while len(result) > 1:
             a = heapq.heappop(result)
             b = heapq.heappop(result)
-            print(f"A + {a}")
-            print(f"B + {b}")
             if b[0] <= a[1]:
                 outcome.append([max(a[0], b[0]), min(a[1], b[1])])
             heapq.heappush(result, [b[0], max(a[1], b[1])])
