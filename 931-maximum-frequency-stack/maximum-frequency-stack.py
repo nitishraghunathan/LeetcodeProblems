2ebf7c92b8886fe86915a7f72e2e4f5bca72b20b
nitishraghunathan <nitish.raghunathan1994@gmail.com>
2024-03-23 19:13:17 -0700

    Time: 227 ms (78.99%) | Memory: 24.6 MB (99.54%) - LeetSync

--- 931-maximum-frequency-stack/maximum-frequency-stack.py
+++ 931-maximum-frequency-stack/maximum-frequency-stack.py
@@ -1,33 +1,32 @@
 class FreqStack:
+
     def __init__(self):
-        self.number_dict = {}
-        self.frequency_counter = [[]]
+        self.map_dict = {}
+        self.max_count = 0
+        self.group = []
+        
 
     def push(self, val: int) -> None:
-        number_dict = self.number_dict
-        frequency_counter = self.frequency_counter 
-        if val not in number_dict:
-            number_dict[val] = 0
-        number_dict[val] += 1
-        if number_dict[val] > len(frequency_counter)-1:
-            frequency_counter.append([])
-        frequency_counter[number_dict[val]].append(val)
+        if val not in self.map_dict:
+            self.map_dict[val] = 0
+        self.map_dict[val] +=1
+        if self.map_dict[val] > self.max_count:
+            self.max_count = self.map_dict[val]
+        if self.max_count > len(self.group):
+            self.group.append([])
+        self.group[self.map_dict[val]-1].append(val)        
+
     def pop(self) -> int:
-        number_dict = self.number_dict
-        frequency_counter = self.frequency_counter 
-        length = len(frequency_counter)
-        if  length== 1:
-            return -1
-        val =-1
-        if len(frequency_counter[length-1]) > 0:
-            val = frequency_counter[length-1].pop()
-            number_dict[val] -=1
-            if number_dict[val] == 0:
-                number_dict.pop(val)
-        if not frequency_counter[length-1]:
-            frequency_counter.pop()
-        return val
-        
+       key = -1
+       if self.group:
+            key = self.group[self.max_count-1].pop()
+       self.map_dict[key] -=1
+       if self.map_dict[key] == 0:
+            del self.map_dict[key]
+       if not self.group[self.max_count-1]:
+            self.group.pop()
+            self.max_count-=1
+       return key
         
 
 
