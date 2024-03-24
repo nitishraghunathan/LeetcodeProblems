d76b66ad31e21ebb7cc767d2d5d05d7bdaba6fe8
nitishraghunathan <nitish.raghunathan1994@gmail.com>
2024-03-24 09:44:59 -0700

    Time: 77 ms (50.08%) | Memory: 19.1 MB (36.24%) - LeetSync

--- 348-design-tic-tac-toe/design-tic-tac-toe.py
+++ 348-design-tic-tac-toe/design-tic-tac-toe.py
@@ -1,34 +1,32 @@
 class TicTacToe:
-    def __init__(self, n: int):
-        ## RC ##
-        ## APPROACH : GREEDY ##
-        ## Similar to leetcode: 36. Valid Sudoku ##
-        
-		## TIME COMPLEXITY : O(1) ##
-		## SPACE COMPLEXITY : O(N) ##
 
-        self.rows = [0] * n         # store total sums by row
-        self.cols = [0] * n         # store total sums by column
-        self.diagonal = 0           # store total sums by diagonal i.e (row == col)
-        self.anti_diagonal = 0      # store total sums by anti-diagonal i.e (row == n-col-1)
+    def __init__(self, n: int):
+        self.rows = [0]*n
+        self.cols = [0]*n
+        self.negative_diagnol = 0
+        self.positive_diagnol = 0
         self.n = n
         
 
     def move(self, row: int, col: int, player: int) -> int:
-        value = 1 if(player == 1) else -1
-        
-        self.rows[row] += value
-        self.cols[col] += value
-        
-        if(row == col):
-            self.diagonal += value
-        if(row == (self.n - 1) - col):                  # Anti diagonal
-            self.anti_diagonal += value
-                    
-        if(self.rows[row] == -self.n or self.cols[col] == -self.n or self.anti_diagonal == -self.n or self.diagonal == -self.n):
-            return 2
-        
-        if(self.rows[row] == self.n or self.cols[col] == self.n or self.anti_diagonal == self.n or self.diagonal == self.n):
+        move = 1 if player == 1 else -1
+        self.rows[row] += move
+        self.cols[col] += move 
+        if row==col:
+            self.negative_diagnol += move 
+        if row+col ==self.n-1:
+            self.positive_diagnol += move 
+        if self.rows[row] == self.n or self.cols[col]==self.n or self.positive_diagnol == self.n or self.negative_diagnol == self.n:
             return 1
-        
+        if self.rows[row] == -self.n or self.cols[col]==-self.n or self.positive_diagnol == -self.n or self.negative_diagnol == -self.n:
+            return 2
         return 0
+        
+
+
+        
+
+
+# Your TicTacToe object will be instantiated and called as such:
+# obj = TicTacToe(n)
+# param_1 = obj.move(row,col,player)
