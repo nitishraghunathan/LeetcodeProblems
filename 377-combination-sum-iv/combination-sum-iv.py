60b747d872c5e60f37f6ded3c5fa48d47f860fa4
nitishraghunathan <nitish.raghunathan1994@gmail.com>
2024-03-25 20:11:35 -0700

    Time: 47 ms (19.87%) | Memory: 16.5 MB (60.57%) - LeetSync

---
+++ 377-combination-sum-iv/combination-sum-iv.py
@@ -0,0 +1,21 @@
+class Solution:
+    def combinationSum4(self, nums: List[int], target: int) -> int:
+        def recursion(nums, index, target, sum_value, result, fist):
+            if sum_value == target:
+                result.append(list(fist))
+            if index == len(nums):
+                return result
+
+            result = recursion(nums, index+1, target, sum_value + nums[index], result, fist + [nums[index]])
+            result = recursion(nums, index+1, target, sum_value, result, fist)
+            return result
+                
+
+        dp = [0]*(target+1)
+        dp[0] = 1
+        for i in range(1,len(dp)):
+            for j in range(len(nums)):
+                if nums[j] <= i:
+                    dp[i] += dp[i-nums[j]]
+        print(dp)
+        return dp[-1]
