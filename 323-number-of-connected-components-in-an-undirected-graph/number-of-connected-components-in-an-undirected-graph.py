4186922296a27083b3e6bbda9bee2a61966f3d02
nitishraghunathan <nitish.raghunathan1994@gmail.com>
2024-03-26 19:40:49 -0700

    Time: 97 ms (23.76%) | Memory: 18.4 MB (53.24%) - LeetSync

--- 323-number-of-connected-components-in-an-undirected-graph/number-of-connected-components-in-an-undirected-graph.py
+++ 323-number-of-connected-components-in-an-undirected-graph/number-of-connected-components-in-an-undirected-graph.py
@@ -1,33 +1,28 @@
 class UnionFind:
-    def __init__(self, x):
-        self.root = [i for i in range(x)]
-        self.rank = [1]*x
-        
-    def find(self, x):
+    def __init__(self, n):
+        self.rank = [1 for i in range(n)]
+        self.root = [i for i in range(n)]
+    def find(self,x):
         if x == self.root[x]:
             return x
         self.root[x] = self.find(self.root[x])
         return self.root[x]
-    
-    def union(self, x, y):
-        RootX = self.find(x)
-        RootY = self.find(y)
-        if RootX != RootY:
-            if self.rank[RootX] > self.rank[RootY]:
-                self.root[RootY] = RootX
-            elif self.rank[RootY] > self.rank[RootX]:
-                self.root[RootX] = RootY
+    def union(self,x,y):
+        root_x = self.find(x)
+        root_y = self.find(y)
+        if root_x!=root_y:
+            if self.rank[root_x] >= self.rank[root_y]:
+                self.root[root_y] = root_x
+                self.rank[root_x] +=1
             else:
-                self.root[RootY] = RootX
-                self.rank[RootX] +=1
-                            
+                self.root[root_x] = root_y
+                self.rank[root_y] +=1
 class Solution:
     def countComponents(self, n: int, edges: List[List[int]]) -> int:
-        uf = UnionFind(n)
-        edges = sorted(edges, key= lambda edges: edges[0])
+        uf = UnionFind(n) 
+        edges = sorted(edges, key=lambda x:x[0])
         for edge in edges:
             uf.union(edge[0], edge[1])
         for i in range(n):
             uf.find(i)
         return len(set(uf.root))
-
