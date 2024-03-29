5737627bec77f46ed0de4d88a890ccba6855eaaa
nitishraghunathan <nitish.raghunathan1994@gmail.com>
2024-03-28 21:48:18 -0700

    Time: 1599 ms (53.65%) | Memory: 73.5 MB (52.50%) - LeetSync

--- 1300-critical-connections-in-a-network/critical-connections-in-a-network.py
+++ 1300-critical-connections-in-a-network/critical-connections-in-a-network.py
@@ -8,17 +8,17 @@ class Solution:
             graph[connection[0]].append(connection[1])
             graph[connection[1]].append(connection[0])
         low = [n]*n
-        critical_connections = []
+        result = []
         def dfs(src, discovery_time, parent):
             if low[src] == n:
                 low[src] = discovery_time
                 for i in graph[src]:
-                    if i !=parent:
+                    if i != parent:
                         expected_time = discovery_time+1
                         actual_time = dfs(i, expected_time, src)
                         if actual_time >= expected_time:
-                            critical_connections.append([src,i])
+                            result.append([src,i])
                         low[src] = min(low[src], low[i])
             return low[src]
         dfs(n-1,0,-1)
-        return critical_connections
+        return result
