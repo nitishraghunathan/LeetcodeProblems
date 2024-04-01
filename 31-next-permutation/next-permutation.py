bfec5f5ae3549d8c91ccd64b5651517ce0324e3b
nitishraghunathan <nitish.raghunathan1994@gmail.com>
2024-03-31 23:16:12 -0700

    Time: 53 ms (5.82%) | Memory: 16.6 MB (13.96%) - LeetSync

--- 31-next-permutation/next-permutation.py
+++ 31-next-permutation/next-permutation.py
@@ -2,6 +2,9 @@ class Solution:
     def nextPermutation(self, nums: List[int]) -> None:
         """
         Do not return anything, modify nums in-place instead.
+        1. Find the first left element less than its previous right element
+        2. I findex greater than -1 find the smallest right element greater than the left element
+        3. swap them and then reverse the string
         """
         j = len(nums)-2
         while j > -1 and nums[j] >= nums[j+1]:
