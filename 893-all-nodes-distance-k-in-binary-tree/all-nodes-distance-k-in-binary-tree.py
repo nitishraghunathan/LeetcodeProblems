2e4fa276c2fe1bea30d67b0658192b20b6f9ee14
nitishraghunathan <nitish.raghunathan1994@gmail.com>
2024-04-13 17:44:09 -0700

    Time: 38 ms (68.42%) | Memory: 16.9 MB (31.42%) - LeetSync

--- 893-all-nodes-distance-k-in-binary-tree/all-nodes-distance-k-in-binary-tree.py
+++ 893-all-nodes-distance-k-in-binary-tree/all-nodes-distance-k-in-binary-tree.py
@@ -1,35 +1,42 @@
 class Solution:
     def distanceK(self, root: TreeNode, target: TreeNode, k: int) -> List[int]:
-        graph = collections.defaultdict(list)
-        # Recursively build the undirected graph from the given binary tree.
-        def build_graph(cur, parent):
-            if cur and parent:
-                graph[cur.val].append(parent.val)
-                graph[parent.val].append(cur.val)
-            if cur.left:
-                build_graph(cur.left, cur)
-            if cur.right:
-                build_graph(cur.right, cur) 
-        build_graph(root, None)
-        
-        answer = []
-        visited = set([target.val])
+        """
+        Construct a graph from teh tree and find all nodes at distance K
+        """
+        graph_dict = {}
+        def dfs_graph_construct(root, parent):
+            if not root:
+                return
+            if parent:
+                if root.val not in graph_dict:
+                    graph_dict[root.val] = []
+                if parent.val not in graph_dict:
+                    graph_dict[parent.val] = []
+                graph_dict[root.val].append(parent.val)
+                graph_dict[parent.val].append(root.val)
+            dfs_graph_construct(root.left, root)
+            dfs_graph_construct(root.right, root)
+            return
+        dfs_graph_construct(root, None)
+        queue = []
+        visited = set()
+        result = []
+        queue.append(target.val)
+        visited.add(target.val)
+        while queue and k > -1:
+            size = len(queue)
+            for i in range(size):
+                node = queue.pop(0)
+                if k == 0:
+                    result.append(node)
+                for neighbor in graph_dict.get(node, []):
+                    if neighbor not in visited:
+                        visited.add(neighbor)
+                        queue.append(neighbor)
+            k-=1
+            if k == -1:
+                return result
+        return result
 
-        # Add the target node to the queue with a distance of 0
-        queue = collections.deque([(target.val, 0)])
-        while queue:
-            cur, distance = queue.popleft()
 
-            # If the current node is at distance k from target,
-            # add it to the answer list and continue to the next node.
-            if distance == k:
-                answer.append(cur)
-                continue
-
-            # Add all unvisited neighbors of the current node to the queue.
-            for neighbor in graph[cur]:
-                if neighbor not in visited:
-                    visited.add(neighbor)
-                    queue.append((neighbor, distance + 1))
-                    
-        return answer
+            
