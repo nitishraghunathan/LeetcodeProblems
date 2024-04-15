583f8b17c896e50678c01d6dd9090d0e437947b2
nitishraghunathan <nitish.raghunathan1994@gmail.com>
2024-04-14 17:26:45 -0700

    Time: 2610 ms (8.35%) | Memory: 48.3 MB (39.98%) - LeetSync

--- 833-bus-routes/bus-routes.py
+++ 833-bus-routes/bus-routes.py
@@ -1,22 +1,31 @@
 class Solution:
     def numBusesToDestination(self, routes: List[List[int]], source: int, target: int) -> int:
-        m = defaultdict(set)
-        for i, route in enumerate(routes):
-            for node in route:
-                m[node].add(i)
-        ans = -1
-        vis = set()
-        queue = deque()
+        """
+        1. Add all the buses for every stop in a set.
+        2. use bfs add the source bus stop
+        3. Have a visited set to add the buses travelled and check if bus has beem taken
+        4. Have a counter to calculate the number of buses needed to reach the target
+        """
+        bus_dict = {}
+        for index, route in enumerate(routes): 
+            for stop in route:
+                if stop not in bus_dict:
+                    bus_dict[stop] = set()
+                bus_dict[stop].add(index)
+        visited = set()
+        queue = []
         queue.append(source)
+        bus_no = 0
         while queue:
-            l = len(queue)
-            ans += 1
-            for _ in range(l):
-                cur = queue.popleft()
-                if cur == target:
-                    return ans
-                for bus in m[cur]:
-                    if bus not in vis:
-                        vis.add(bus)
+            size = len(queue)
+            for i in range(size):
+                stop = queue.pop(0)
+                if target == stop:
+                    return bus_no
+                for bus in bus_dict.get(stop,[]):
+                    if bus not in visited:
+                        visited.add(bus)
                         queue.extend(routes[bus])
+            bus_no +=1
         return -1
+                
