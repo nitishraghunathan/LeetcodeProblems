1dd104dc1e974b871d1766c8814c670b8a5db9c1
nitishraghunathan <nitish.raghunathan1994@gmail.com>
2024-04-14 20:59:34 -0700

    Time: 620 ms (56.53%) | Memory: 22.1 MB (64.57%) - LeetSync

--- 525-contiguous-array/contiguous-array.py
+++ 525-contiguous-array/contiguous-array.py
@@ -1,14 +1,13 @@
 class Solution:
     def findMaxLength(self, nums: List[int]) -> int:
         map_dict = {0:-1}
+        max_value = float('-inf')
         total_sum = 0
-        max_value = 0
-        for index, value in enumerate(nums):
-            total_sum = total_sum +1 if value ==1 else total_sum-1
+        for index, num in enumerate(nums):
+            val = 1 if num == 1 else -1
+            total_sum += val
             if total_sum not in map_dict:
                 map_dict[total_sum] = index
             else:
-                max_value = max(max_value, index -  map_dict[total_sum])
-        return max_value
-
-
+                max_value = max(max_value, index-map_dict[total_sum])
+        return 0 if max_value == float('-inf') else max_value
