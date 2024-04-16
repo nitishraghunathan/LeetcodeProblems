a8e28abbdb9c8f779b0808f9c96f7e73c06cdc6e
nitishraghunathan <nitish.raghunathan1994@gmail.com>
2024-04-16 14:06:31 -0700

    Time: 30 ms (87.78%) | Memory: 16.5 MB (75.00%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.py
+++ 20-valid-parentheses/valid-parentheses.py
@@ -1,20 +1,13 @@
 class Solution:
     def isValid(self, s: str) -> bool:
-        """
-        1. Define a dictionary with matching bracket types 
-        2. Create a stack and all opening brackets 
-        3. Once we confront a  closing bracket, popnthe elemtn from the stack and check if the bracket matches 
-        4. If a match is not found return False
-        5. Continue till the string is complete 
-        6. Return if stack is empty to match rule 3 and 1
-        """
-        bracket_dict= {')' : '(', ']' :'[', '}' : '{'}
-        bracket_stack = []
-        for bracket in s:
-            if bracket in bracket_dict:
-                compare_char = bracket_stack.pop() if bracket_stack else None
-                if compare_char != bracket_dict.get(bracket):
-                    return False
+        map_dict = {')' : '(', '}': '{', ']':'['}
+        stack = []
+        for value in s:
+            if value == '(' or value == '{' or value =='[':
+                stack.append(value)
             else:
-                bracket_stack.append(bracket)
-        return not bracket_stack
+                if stack and stack[-1] == map_dict[value]:
+                    stack.pop()
+                else:
+                    return False
+        return not stack
