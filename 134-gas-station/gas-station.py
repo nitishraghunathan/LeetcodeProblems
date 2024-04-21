550cb6a6b0e5672a6e62910bc7690ddbecedb7c3
nitishraghunathan <nitish.raghunathan1994@gmail.com>
2024-04-21 10:21:43 -0700

    Time: 924 ms (82.80%) | Memory: 21.9 MB (98.53%) - LeetSync

--- 134-gas-station/gas-station.py
+++ 134-gas-station/gas-station.py
@@ -1,5 +1,13 @@
 class Solution:
     def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
+        """
+        check is sum of gas is less than cost 
+        if yes then return no solution else there is atleast one solution 
+        keep a total and start index 
+        keep adding total with difference between gas and cost 
+        if total is negative we  know that the start index is probably the position ahead of the current one
+        keep doing it till we get a positive total 
+        """
         if sum(gas) < sum(cost):
             return -1
         total_cost = 0
