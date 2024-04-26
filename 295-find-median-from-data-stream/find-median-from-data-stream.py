3c49c45a6c124908902743831f6f302a6a888b26
nitishraghunathan <nitish.raghunathan1994@gmail.com>
2024-04-26 12:25:42 -0700

    Time: 402 ms (29.52%) | Memory: 39.1 MB (5.29%) - LeetSync

--- 295-find-median-from-data-stream/find-median-from-data-stream.py
+++ 295-find-median-from-data-stream/find-median-from-data-stream.py
@@ -1,33 +1,27 @@
 class MedianFinder:
-    """
-    In order to optimize this we need to solve in log(n) team for addition to a queue
-    but median of a queue is retrieved in o(1) time 
-    hence better to use priority queue
-    """
 
     def __init__(self):
-        self.large_min_heap = []
         self.small_max_heap = []
-        heapq.heapify(self.large_min_heap)
+        self.large_min_heap = []
         heapq.heapify(self.small_max_heap)
-        
+        heapq.heapify(self.large_min_heap)
 
     def addNum(self, num: int) -> None:
         heapq.heappush(self.small_max_heap, -1*num)
         if self.small_max_heap and self.large_min_heap and -1*self.small_max_heap[0] > self.large_min_heap[0]:
             heapq.heappush(self.large_min_heap, -1*heapq.heappop(self.small_max_heap))
-        if len(self.large_min_heap) - len(self.small_max_heap) > 1:
-            heapq.heappush(self.small_max_heap, -1*heapq.heappop(self.large_min_heap))
-        if len(self.small_max_heap) - len(self.large_min_heap) > 1:
+        if len(self.small_max_heap) -len(self.large_min_heap) > 1:
             heapq.heappush(self.large_min_heap, -1*heapq.heappop(self.small_max_heap))
-        
+        if len(self.large_min_heap) -len(self.small_max_heap) > 1:
+            heapq.heappush(self.small_max_heap, -1*heapq.heappop(self.large_min_heap))
+
         
 
     def findMedian(self) -> float:
         if len(self.large_min_heap) == len(self.small_max_heap):
             return ((self.small_max_heap[0]*-1) + self.large_min_heap[0])/2
         return float(self.small_max_heap[0]*(-1)) if len(self.small_max_heap) > len(self.large_min_heap) else float(self.large_min_heap[0])
-        
+
 
 
 # Your MedianFinder object will be instantiated and called as such:
