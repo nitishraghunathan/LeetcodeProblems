b47421775d04703b0f4223883c1d911de2224452
nitishraghunathan <nitish.raghunathan1994@gmail.com>
2024-04-26 11:25:55 -0700

    Time: 231 ms (51.76%) | Memory: 19.5 MB (39.21%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.py
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.py
@@ -1,13 +1,13 @@
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
-        map_dict = {0:1}
-        total_sum = 0
+        map_dict = {0: 1}
+        total_number_arrays= 0
         prefix_sum = 0
         for num in nums:
-            prefix_sum += num 
-            if prefix_sum - k in map_dict:
-                total_sum += map_dict[prefix_sum-k]
+            prefix_sum += num
+            if prefix_sum-k in map_dict:
+                total_number_arrays += map_dict[prefix_sum -k]
             if prefix_sum not in map_dict:
                 map_dict[prefix_sum] = 0
-            map_dict[prefix_sum] +=1
-        return total_sum
+            map_dict[prefix_sum] += 1
+        return total_number_arrays
