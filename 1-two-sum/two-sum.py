6c23b91fb7c0c0abd3ce36ea0fe416f5df2e24b2
nitishraghunathan <nitish.raghunathan1994@gmail.com>
2024-05-31 23:36:46 -0700

    Time: 66 ms (44.56%) | Memory: 17.7 MB (39.40%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -1,9 +1,9 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        two_dict = {}
-        for index, value in enumerate(nums):
-            if target - value in two_dict:
-                return [two_dict[target-value], index]
-            else:
-                two_dict[value] = index
+        map_dict = {}
+        for index,value in enumerate(nums):
+            if target - value in map_dict:
+                return [map_dict[target - value], index]
+            if value not in map_dict:
+                map_dict[value] = index
         return [-1,-1]
