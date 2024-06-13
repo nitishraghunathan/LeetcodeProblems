1fcec5571cfca35916b41b3493ee7f992a319ea6
nitishraghunathan <nitish.raghunathan1994@gmail.com>
2024-06-12 23:11:15 -0700

    Time: 533 ms (67.58%) | Memory: 16.8 MB (80.66%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.py
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.py
@@ -1,19 +1,13 @@
 class Solution:
     def canPartition(self, nums: List[int]) -> bool:
-        # find sum of array elements
         total_sum = sum(nums)
-
-        # if total_sum is odd, it cannot be partitioned into equal sum subsets
-        if total_sum % 2 != 0:
+        if total_sum%2 == 1:
             return False
-        subset_sum = total_sum // 2
-
-        # construct a dp table of size (subset_sum + 1)
-        dp = [False] * (subset_sum + 1)
+        half_sum = total_sum//2
+        dp = [False]*(half_sum+1)
         dp[0] = True
         for curr in nums:
-            for j in range(subset_sum, curr - 1, -1):
+            for j in range(half_sum, curr - 1, -1):
                 dp[j] = dp[j] or dp[j - curr]
-
-        return dp[subset_sum]
-        
+        print(dp)
+        return dp[-1]
