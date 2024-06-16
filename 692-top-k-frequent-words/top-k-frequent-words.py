e334c002095838750afae2709b4b05b502ee7859
nitishraghunathan <nitish.raghunathan1994@gmail.com>
2024-06-15 21:37:30 -0700

    Time: 64 ms (18.34%) | Memory: 16.7 MB (62.48%) - LeetSync

---
+++ 692-top-k-frequent-words/top-k-frequent-words.py
@@ -0,0 +1,18 @@
+class Solution:
+    def topKFrequent(self, words: List[str], k: int) -> List[str]:
+        map_dict = {}
+        for word in words:
+            if word not in map_dict:
+                map_dict[word] = 0
+            map_dict[word] +=1
+        result = []
+        queue = []
+        for key,value in map_dict.items():
+            heapq.heappush(result, (-value, key))
+        while k > 0:
+            value, key = heapq.heappop(result)
+            queue.append(key)
+            k-=1
+        return queue
+
+
