53bbdf9d0dad932d80e14920faecef007db24e31
nitishraghunathan <nitish.raghunathan1994@gmail.com>
2024-06-16 22:18:13 -0700

    Time: 88 ms (23.67%) | Memory: 17.9 MB (51.14%) - LeetSync

--- 735-asteroid-collision/asteroid-collision.py
+++ 735-asteroid-collision/asteroid-collision.py
@@ -1,24 +1,15 @@
 class Solution:
     def asteroidCollision(self, asteroids: List[int]) -> List[int]:
-        """
-        1. If the asteroid is positive, it is moving towards right.
-        2. if the asteroid is negative, it is moving towards left.
-        3. We are going to use a stack to store all asteroids.
-        4. We will pop the stack elements if there is negative and postive asteroid else we continue adding elements to the stack.
-        5. When there is a collison, we take the greatest asteroid if the size is unequal else we assume the asteroids destroy themselves.
-        """
         stack = []
         for asteroid in asteroids:
             stack.append(asteroid)
-            while len(stack) > 1 and (stack[-1] < 0 and stack[-2] > 0):
-                first = stack.pop()
-                second = stack.pop()
-                abs_first = abs(first)
-                abs_second = abs(second)
-                if abs_first == abs_second:
-                    continue 
-                elif abs_first > abs_second:
-                    stack.append(first)
+            while len(stack) > 1 and stack[-1] < 0 and stack[-2] > 0:
+                one = stack.pop()
+                two = stack.pop()
+                if abs(one) > abs(two):
+                    stack.append(one)
+                elif abs(two) > abs(one):
+                    stack.append(two)
                 else:
-                    stack.append(second)
+                    continue
         return stack
