79e1461329580d0af07dceef0eacf9930e3118d5
nitishraghunathan <nitish.raghunathan1994@gmail.com>
2024-06-17 10:52:26 -0700

    Time: 32 ms (86.82%) | Memory: 16.7 MB (75.89%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.py
+++ 22-generate-parentheses/generate-parentheses.py
@@ -6,17 +6,19 @@ class Solution:
         3. Consider nesting cases
         4. perfrom recursion, backtrack and generate combinations
         """
-        def generator(n:int, string:str, result: List[str], start:int, end:int)->List[str]:
+        result = []
+        def generate(open, close, string):
+            nonlocal result, n
             if len(string) == 2*n:
                 result.append(string)
                 return result
-            if start > 0:
-                string +='('
-                result = generator(n, string, result, start-1, end)
-                string = string[0:len(string)-1]
-            if end > start:
+            if open > 0:
+                string += '('
+                result = generate(open-1, close, string)
+                string = string[:len(string)-1]
+            if close  > open:
                 string += ')'
-                result = generator(n,string, result, start, end-1)
+                result = generate(open, close-1, string)
             return result
-        return generator(n, '', [], n, n)
+        return generate(n, n, '')
         
