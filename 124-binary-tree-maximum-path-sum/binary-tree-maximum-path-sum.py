4999ba8d95e545b1018a6101b810de4f3afcaa8e
nitishraghunathan <nitish.raghunathan1994@gmail.com>
2024-06-17 22:30:47 -0700

    Time: 74 ms (11.95%) | Memory: 20.9 MB (66.93%) - LeetSync

--- 124-binary-tree-maximum-path-sum/binary-tree-maximum-path-sum.py
+++ 124-binary-tree-maximum-path-sum/binary-tree-maximum-path-sum.py
@@ -5,6 +5,14 @@
 #         self.left = left
 #         self.right = right
 class Solution:
+
+    """
+    There are three use cases in this scenairos 
+    1. Return value = max of left and right + root.val 
+    2. Root value 
+    3. path within subtree -> root.val, left, right 
+    4. When we return value max of return value + root.value
+    """
     def maxPathSum(self, root: Optional[TreeNode]) -> int:
         max_value = float('-inf')
         def dfs(root):
@@ -14,7 +22,7 @@ class Solution:
             left = dfs(root.left)
             right = dfs(root.right)
             return_value = root.val + max(left, right)
-            max_value = max(max_value, max(return_value , max(root.val, left + right + root.val)))
+            max_value = max(max_value, max(return_value , root.val, left + right + root.val))
             return max(root.val, return_value)
         max_value = max(dfs(root), max_value)
         return max_value
