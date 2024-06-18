24d55e8f74af9b96a5513f62961502e19affc497
nitishraghunathan <nitish.raghunathan1994@gmail.com>
2024-06-17 21:53:21 -0700

    Time: 51 ms (5.16%) | Memory: 16.5 MB (35.84%) - LeetSync

--- 394-decode-string/decode-string.py
+++ 394-decode-string/decode-string.py
@@ -1,24 +1,19 @@
 class Solution:
     def decodeString(self, s: str) -> str:
-        """
-        1. Add all the strings to a stack one by one till we reach a closing brace
-        2. Pop all elements once we reach the closing brace till we have opening brace
-        3. Pop the opening brace
-        4. Pop the strings as long as they are digits
-        5. convert the finalpopped numbers into an int and mulitply with the first popped string and then append it back to the stack
-        7. Finally join all the strings in the stack
-        """
         stack = []
-        for i in s:
-            if i != ']':
-                stack.append(i)
+        for string in s:
+            if string != ']':
+                stack.append(string)
             else:
-                sub_str = ''
-                while stack[-1] != '[':
-                    sub_str = stack.pop() +sub_str
+                substring = ''
+                while stack and stack[-1]!='[':
+                    substring = stack.pop() + substring
                 stack.pop()
-                number= ''
+                number = ''
                 while stack and stack[-1].isdigit():
                     number = stack.pop() + number
-                stack.append(int(number)*sub_str)
-        return ''.join(stack)
+                stack.append(int(number)*substring)
+        substring =''
+        while stack:
+            substring = stack.pop() + substring
+        return substring
