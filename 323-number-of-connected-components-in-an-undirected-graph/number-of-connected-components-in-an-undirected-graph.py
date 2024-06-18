18400e3c617f195f11229cc91e843d0687ae324b
nitishraghunathan <nitish.raghunathan1994@gmail.com>
2024-06-17 21:40:25 -0700

    Time: 99 ms (14.06%) | Memory: 18.2 MB (70.26%) - LeetSync

--- 323-number-of-connected-components-in-an-undirected-graph/number-of-connected-components-in-an-undirected-graph.py
+++ 323-number-of-connected-components-in-an-undirected-graph/number-of-connected-components-in-an-undirected-graph.py
@@ -1,28 +1,33 @@
-class UnionFind:
-    def __init__(self, n):
-        self.rank = [1 for i in range(n)]
-        self.root = [i for i in range(n)]
-    def find(self,x):
-        if x == self.root[x]:
-            return x
-        self.root[x] = self.find(self.root[x])
-        return self.root[x]
-    def union(self,x,y):
-        root_x = self.find(x)
-        root_y = self.find(y)
-        if root_x!=root_y:
-            if self.rank[root_x] >= self.rank[root_y]:
-                self.root[root_y] = root_x
-                self.rank[root_x] +=1
-            else:
-                self.root[root_x] = root_y
-                self.rank[root_y] +=1
 class Solution:
     def countComponents(self, n: int, edges: List[List[int]]) -> int:
-        uf = UnionFind(n) 
-        edges = sorted(edges, key=lambda x:x[0])
+        uf = UnionFind(n)
+        edges = sorted(edges)
         for edge in edges:
             uf.union(edge[0], edge[1])
         for i in range(n):
             uf.find(i)
         return len(set(uf.root))
+
+
+class UnionFind:
+    def __init__(self, n):
+        self.root = [i for i in range(n)]
+        self.rank = [1]*n
+    def find(self, x):
+        if self.root[x] == x:
+            return self.root[x]
+        self.root[x] =  self.find(self.root[x])
+        return self.root[x]
+    def union(self, x, y):
+        rootx = self.find(x)
+        rooty = self.find(y)
+        if rootx!=rooty:
+            if self.rank[rootx] >= self.rank[rooty]:
+                self.root[rooty] = rootx
+                self.rank[rootx] +=1
+            else:
+                self.root[rootx] = rooty
+                self.rank[rooty] +=1
+        
+
+            
