ef52032e961049f886ba3ab6e1e3391bb7458a69
nitishraghunathan <nitish.raghunathan1994@gmail.com>
2024-11-09 19:48:28 -0800

    Time: 371 ms (5.03%) | Memory: 18.1 MB (85.76%) - LeetSync

--- 207-course-schedule/course-schedule.py
+++ 207-course-schedule/course-schedule.py
@@ -1,22 +1,33 @@
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        map_dict ={}
+        """
+        1. Create a map of all courses and it depedencies
+        2. Create a queue and add all the courses with zero depenncies 
+        3. Iterate the map and use every element in the queue to remove dependencies
+        4. There can be multiple orders
+        5. remove each course from the map depdency as we are ordering them. 
+        6. once any course has no more depdencies add them to the queue and find its dependent courses
+        7. After completing the iteration add the element to the result
+        8. Once all the queue elements are evaluated if the len of result != numOfCourses then we cannot form a course schedule 
+        9. Otherwise we can
+        """
+        course_dictionary = {}
         for i in range(numCourses):
-            if i not in map_dict:
-                map_dict[i] = set()
-        for pre in prerequisites:
-            map_dict[pre[1]].add(pre[0])
+            if i not in course_dictionary:
+                course_dictionary[i] = set()
+        for req in prerequisites:
+            course_dictionary[req[0]].add(req[1])
         queue = []
-        for key, value in map_dict.items():
+        for key, value in course_dictionary.items():
             if not value:
                 queue.append(key)
         result = []
         while queue:
-            id = queue.pop(0)
-            result.insert(0, id)
-            for key, value in map_dict.items():
-                if id in value:
-                    value.remove(id)
+            course_id = queue.pop(0)
+            result.append(course_id)
+            for key, value in course_dictionary.items():
+                if course_id in value:
+                    value.remove(course_id)
                     if not value:
                         queue.append(key)
-        return len(result)==numCourses
+        return len(result) == numCourses
