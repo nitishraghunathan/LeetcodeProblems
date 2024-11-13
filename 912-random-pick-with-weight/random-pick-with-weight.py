89d206cdd893ca31939da3eb596c6adc447198a6
nitishraghunathan <nitish.raghunathan1994@gmail.com>
2024-11-12 19:37:01 -0800

    Time: 73 ms (56.37%) | Memory: 22 MB (11.10%) - LeetSync

--- 912-random-pick-with-weight/random-pick-with-weight.py
+++ 912-random-pick-with-weight/random-pick-with-weight.py
@@ -1,37 +1,21 @@
 class Solution:
+
     def __init__(self, w: List[int]):
-        """
-        :type w: List[int]
-        create a prefix sum:
-        add all the weights and store prefix sum array 
-        have a total_sum array 
-        when comupting weighted random 
-        use total_sum * random.random 
-        ince you get the weight 
-        find the index closest to the weight 
-        use low = 0 and high = len(w)-1
-        and then if target < w[mid] reduce the right window else 
-        increase the left window
-        """
-        #Accumulate all the wieghts and find the index closest to the weight
-        self.prefix_sums = []
-        prefix_sum = 0
-        for weight in w:
-            prefix_sum += weight
-            self.prefix_sums.append(prefix_sum)
-        self.total_sum = prefix_sum
+        self.prefix_total_sum = []
+        self.prefix_sum = 0
+        for weights in w:
+            self.prefix_sum += weights
+            self.prefix_total_sum.append(self.prefix_sum)
+
 
     def pickIndex(self) -> int:
-        """
-        :rtype: int
-        """
-        target = self.total_sum * random.random()
-        # run a binary search to find the target zone
-        low, high = 0, len(self.prefix_sums)
+
+        target = self.prefix_sum*random.random()
+        low, high = 0, len(self.prefix_total_sum)
         while low < high:
-            mid = low + (high - low) // 2
-            if target > self.prefix_sums[mid]:
-                low = mid + 1
+            mid = low + (high-low)//2
+            if self.prefix_total_sum[mid] < target:
+                low = mid+1
             else:
                 high = mid
         return low
