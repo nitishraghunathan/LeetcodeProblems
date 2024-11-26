0a12aafa4ed7132b9e65866632eaf1a77ed8d281
nitishraghunathan <nitish.raghunathan1994@gmail.com>
2024-11-26 06:10:36 -0800

    Time: 44 ms (11.69%) | Memory: 26.6 MB (5.23%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.py
+++ 238-product-of-array-except-self/product-of-array-except-self.py
@@ -1,14 +1,22 @@
 class Solution:
-    def productExceptSelf(self, nums: List[int]) -> List[int]: 
-        result = [1]*(len(nums))
-        prefix = 1
-        for i in range(len(nums)):
-            result[i] *= prefix
-            prefix *=nums[i]
-            
-        postfix = 1
-        for i in range(len(nums)-1, -1,-1):
-            result[i] *= postfix
-            postfix *= nums[i]
-            
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        size = len(nums)
+        left_array = [0]*size
+        right_array = [0]*size
+        result = [0]*size
+        left_array[0] = nums[0]
+        right_array[size-1] = nums[size-1]
+
+        for i in range(1, size):
+            left_array[i] = nums[i]*left_array[i-1]
+        for j in range(len(nums)-2, -1, -1):
+            right_array[j] = nums[j]*right_array[j+1] 
+        for i in range(size):
+            if i == 0:
+                result[i] = right_array[i+1]
+            elif i == len(nums) - 1:
+                result[i] = left_array[i-1]
+            else:
+                result[i] = left_array[i-1]*right_array[i+1]
         return result
+        
