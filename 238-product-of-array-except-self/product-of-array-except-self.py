e4ee76ceb838617d9abd4df297ca4a33dd3d8b05
nitishraghunathan <nitish.raghunathan1994@gmail.com>
2024-11-26 06:16:58 -0800

    Time: 25 ms (52.82%) | Memory: 22.7 MB (64.35%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.py
+++ 238-product-of-array-except-self/product-of-array-except-self.py
@@ -1,22 +1,32 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        size = len(nums)
-        left_array = [0]*size
-        right_array = [0]*size
-        result = [0]*size
-        left_array[0] = nums[0]
-        right_array[size-1] = nums[size-1]
 
-        for i in range(1, size):
-            left_array[i] = nums[i]*left_array[i-1]
-        for j in range(len(nums)-2, -1, -1):
-            right_array[j] = nums[j]*right_array[j+1] 
-        for i in range(size):
-            if i == 0:
-                result[i] = right_array[i+1]
-            elif i == len(nums) - 1:
-                result[i] = left_array[i-1]
-            else:
-                result[i] = left_array[i-1]*right_array[i+1]
-        return result
-        
+        # The length of the input array
+        length = len(nums)
+
+        # The answer array to be returned
+        answer = [0] * length
+
+        # answer[i] contains the product of all the elements to the left
+        # Note: for the element at index '0', there are no elements to the left,
+        # so the answer[0] would be 1
+        answer[0] = 1
+        for i in range(1, length):
+
+            # answer[i - 1] already contains the product of elements to the left of 'i - 1'
+            # Simply multiplying it with nums[i - 1] would give the product of all
+            # elements to the left of index 'i'
+            answer[i] = nums[i - 1] * answer[i - 1]
+
+        # R contains the product of all the elements to the right
+        # Note: for the element at index 'length - 1', there are no elements to the right,
+        # so the R would be 1
+        R = 1
+        for i in reversed(range(length)):
+
+            # For the index 'i', R would contain the
+            # product of all elements to the right. We update R accordingly
+            answer[i] = answer[i] * R
+            R *= nums[i]
+
+        return answer
