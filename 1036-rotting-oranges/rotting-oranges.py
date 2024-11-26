ce7fbf5b11d655dbe3a03aafdc02784a13225714
nitishraghunathan <nitish.raghunathan1994@gmail.com>
2024-11-26 06:50:50 -0800

    Time: 3 ms (81.70%) | Memory: 17.6 MB (5.98%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.py
+++ 1036-rotting-oranges/rotting-oranges.py
@@ -1,30 +1,31 @@
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
-        def bfs(queue, grid, counter):
-            directions = [[0,1], [0,-1], [1,0], [-1,0]]
+        counter = -1
+        def bfs(queue, grid):
+            nonlocal counter
+            directions = [[-1,0], [1,0], [0,1], [0,-1]]
             while queue:
                 size = len(queue)
                 for i in range(size):
-                    x, y = queue.pop(0)
-                    for dir in directions:
-                        x_new = x + dir[0]
-                        y_new = y + dir[1]
-                        if x_new < 0 or y_new < 0 or x_new > len(grid)-1 or y_new > len(grid[x_new]) -1 or grid[x_new][y_new] != 1:
+                    x, y  = queue.pop(0)
+                    for direct in directions:
+                        new_x = x + direct[0]
+                        new_y = y + direct[1]
+                        if new_x < 0 or new_x > len(grid)-1 or new_y < 0 or new_y > len(grid[new_x])-1 or grid[new_x][new_y] !=1:
                             continue
-                        grid[x_new][y_new] = 2
-                        queue.append([x_new, y_new])
-                counter+=1
-            return counter
+                        queue.append([new_x, new_y])
+                        grid[new_x][new_y] = 2
+                counter +=1
+
+
         queue = []
-        counter = -1
         for i in range(len(grid)):
             for j in range(len(grid[i])):
                 if grid[i][j] == 2:
                     queue.append([i,j])
-        counter = bfs(queue, grid, counter)
+        bfs(queue, grid)
         for i in range(len(grid)):
             for j in range(len(grid[i])):
                 if grid[i][j] == 1:
                     return -1
         return 0 if counter == -1 else counter
-                
