eddef13852a3e2b0b653040c1aa9b4c5947cd6df
nitishraghunathan <nitish.raghunathan1994@gmail.com>
2024-11-30 08:39:24 -0800

    Time: 11 ms (63.82%) | Memory: 17.6 MB (5.08%) - LeetSync

--- 39-combination-sum/combination-sum.py
+++ 39-combination-sum/combination-sum.py
@@ -1,15 +1,17 @@
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
         result = []
-        def recursion(sum_value, list_numbers,start):
-            if sum_value == 0:
-                result.append(list(list_numbers))
-                return
-            elif sum_value < 0:
+        def recursion(candidates: List[int], target: int, candid_list: List[int], start: int):
+            nonlocal result
+            if target == 0:
+                result.append(list(candid_list))
+                return 
+            elif target < 0:
                 return 
             for i in range(start, len(candidates)):
-                list_numbers.append(candidates[i])
-                recursion(sum_value-candidates[i], list_numbers, i)
-                list_numbers.pop()
-        recursion(target,[],0)
+                candid_list.append(candidates[i])
+                recursion(candidates, target - candidates[i], candid_list, i)
+                candid_list.pop()
+            return 
+        recursion(candidates, target, [], 0)
         return result
