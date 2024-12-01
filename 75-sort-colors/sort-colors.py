eda5d637e71c8a401a3706fcb0f536a1368234b2
nitishraghunathan <nitish.raghunathan1994@gmail.com>
2024-11-30 17:18:12 -0800

    Time: 1 ms (18.52%) | Memory: 17.2 MB (5.19%) - LeetSync

--- 75-sort-colors/sort-colors.py
+++ 75-sort-colors/sort-colors.py
@@ -3,16 +3,23 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        p0 = current = 0
-        p2 = len(nums)-1
-        while current <= p2:
-            if nums[current] == 0:
-                nums[p0], nums[current] = nums[current], nums[p0]
-                current+=1
-                p0+=1
-            elif nums[current] == 2:
-                nums[p2], nums[current] = nums[current], nums[p2]
-                p2-=1
+        """
+        1. Take three pointers, zero, current, two. Zero and current would be at index 0 and two would be the last index of nums
+        2. Iterate through all elements of the array from left to right
+        3. when there is a zero value then swap elements at zero pointer and current pointer and increment both the pointers
+        4. when there is a two value swap elements at current index and two pointer index, decrement two index
+        5. if value is one then increment current index.
+        """
+        current, zero = 0,0
+        two, right = len(nums) - 1, len(nums) -1 
+        while current <= two:
+            if nums[current] == 2:
+                nums[current], nums[two] = nums[two], nums[current]
+                two -=1
+            elif nums[current] == 0:
+                nums[current], nums[zero] = nums[zero], nums[current]
+                current +=1
+                zero+=1
             else:
                 current +=1
-        return nums
+        return nums        
