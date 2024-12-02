3568034198d3fac1453f4b5c041e8aec10c1d91d
nitishraghunathan <nitish.raghunathan1994@gmail.com>
2024-12-02 07:28:53 -0800

    Time: 2 ms (12.81%) | Memory: 17.4 MB (5.89%) - LeetSync

--- 78-subsets/subsets.py
+++ 78-subsets/subsets.py
@@ -1,23 +1,15 @@
 class Solution:
-    def subsets(self, nums: List[int]) -> List[List[int]]:
-        """
-        Thought process
-        recursion:
-        1. Base condition if index > len(nums)-1 add it to the result and return
-        2. Have a for loop which interates through all elements in number
-        3. Either pick one and perform recursion or dont add and perform recursion.
-        4. increase the index by one for every recursive call
-        """
-        result = set()
-        def recursion(nums:list, target_list:list, index:int):
-            if index > len(nums) - 1:
-                result.add(tuple(target_list))
-                return
-            for i in range(index, len(nums)):
-                target_list.append(nums[i])
-                recursion(nums, target_list, i+1)
-                target_list.pop()
-                recursion(nums, target_list, i+1)
-            return 
-        recursion(nums, [], 0)
-        return list(result)
+    def subsets(self, nums):
+        self.output = []
+        self.n, self.k = len(nums), None
+        for self.k in range(self.n + 1):
+            self.backtrack(0, [], nums)
+        return self.output
+
+    def backtrack(self, first, curr, nums):
+        if len(curr) == self.k:
+            self.output.append(curr[:])
+        for i in range(first, self.n):
+            curr.append(nums[i])
+            self.backtrack(i + 1, curr, nums)
+            curr.pop()
