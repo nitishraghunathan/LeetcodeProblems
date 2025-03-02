0dd5e6945a6da7cd484ec8466e1bf2130e45154a
nitishraghunathan <nitish.raghunathan1994@gmail.com>
2025-03-01 17:08:44 -0800

    Time: 0 ms (100.00%) | Memory: 18.8 MB (50.96%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -5,8 +5,6 @@ class Solution:
             if target - value in map_dict:
                 return [map_dict[target-value], index]
             if value not in map_dict:
-                map_dict[value] = index
-
-        return [-1, -1]
-
-        
+                map_dict[value] = -1
+            map_dict[value] = index
+        return [-1,-1]
