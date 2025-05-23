8d4a079852c297934286e52e81e2d20ab602561c
nitishraghunathan <nitish.raghunathan1994@gmail.com>
2025-05-23 15:44:00 -0700

    Time: 19 ms (16.10%) | Memory: 17.7 MB (86.05%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -1,3 +1,15 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        return sorted(s) == sorted(t)
+        if len(s) != len(t):
+            return False
+        map_dict = {}
+        for index, value in enumerate(s):
+            if value not in map_dict:
+                map_dict[value] = 0
+            map_dict[value] +=1
+        for index, value in enumerate(t):
+            if value not in map_dict or map_dict[value] == 0:
+                return False
+            else:
+                map_dict[value] -=1
+        return True
