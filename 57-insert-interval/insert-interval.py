549a84b015a9d933e71a24a71553499556f886bf
nitishraghunathan <nitish.raghunathan1994@gmail.com>
2025-05-25 16:30:17 -0700

    Time: 3 ms (49.69%) | Memory: 19.9 MB (20.12%) - LeetSync

--- 57-insert-interval/insert-interval.py
+++ 57-insert-interval/insert-interval.py
@@ -1,28 +1,18 @@
 class Solution:
     def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
-        result = []
-        final_result = []
-        for interval in intervals:
-            heapq.heappush(result, [interval[0], interval[1]])
-        heapq.heappush(result, [newInterval[0], newInterval[1]])
-        while len(result) > 1:
-            first = heapq.heappop(result)
-            second = heapq.heappop(result)
-            if second[0] > first[1]:
-                final_result.append(first)
-                heapq.heappush(result, second)
-            else:
-                heapq.heappush(result, [min(first[0], second[0]), max(first[1], second[1])])
-        while result:
-            first = heapq.heappop(result)
-            if final_result:
-                if first[0] > final_result[-1][1]:
-                    final_result.append(first)
-                else:
-                    final_result.append(min(first[0], final_result[-1][0]), max(first[1], final_result[-1][1]))
-            else:
-                final_result.append(first)
-        return final_result
-
-
+        i = 0
+        outputs = []
+        while i < len(intervals) and intervals[i][1] < newInterval[0]:
+            outputs.append(intervals[i])
+            i += 1
         
+        while i < len(intervals) and intervals[i][0] <= newInterval[1]:
+            newInterval = [min(intervals[i][0], newInterval[0]), max(intervals[i][1], newInterval[1])]
+            i += 1
+        outputs.append(newInterval)
+
+        while i < len(intervals):
+            outputs.append(intervals[i])
+            i += 1
+        
+        return outputs
