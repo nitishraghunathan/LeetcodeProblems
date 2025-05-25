c86a5df66434055686ca85af01ca37dbd37c011f
nitishraghunathan <nitish.raghunathan1994@gmail.com>
2025-05-25 16:26:35 -0700

    Time: 15 ms (7.39%) | Memory: 20.5 MB (5.50%) - LeetSync

--- 57-insert-interval/insert-interval.py
+++ 57-insert-interval/insert-interval.py
@@ -1,23 +1,28 @@
 class Solution:
     def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
-        heapq.heapify(intervals)
         result = []
-        heapq.heappush(intervals, newInterval)
-        while len(intervals) > 1:
-            first = heapq.heappop(intervals)
-            second = heapq.heappop(intervals)
-            if first[1] >= second[0]:
-                heapq.heappush(intervals, [min(first[0], second[0]), max(first[1], second[1])])
+        final_result = []
+        for interval in intervals:
+            heapq.heappush(result, [interval[0], interval[1]])
+        heapq.heappush(result, [newInterval[0], newInterval[1]])
+        while len(result) > 1:
+            first = heapq.heappop(result)
+            second = heapq.heappop(result)
+            if second[0] > first[1]:
+                final_result.append(first)
+                heapq.heappush(result, second)
             else:
-                result.append(first)
-                heapq.heappush(intervals, second)
-        if len(intervals):
-            last = heapq.heappop(intervals)
-            if result and last[0] <= result[-1][1]:
-                result[-1][0] = min(result[-1][0], last[0])
-                result[-1][1] = max(result[-1][1], last[1])
+                heapq.heappush(result, [min(first[0], second[0]), max(first[1], second[1])])
+        while result:
+            first = heapq.heappop(result)
+            if final_result:
+                if first[0] > final_result[-1][1]:
+                    final_result.append(first)
+                else:
+                    final_result.append(min(first[0], final_result[-1][0]), max(first[1], final_result[-1][1]))
             else:
-                result.append(last)
-        return result
+                final_result.append(first)
+        return final_result
+
 
         
