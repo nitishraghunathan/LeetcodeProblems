4b48671ebbb0cd80d4c064c64a3e9caa0b044e94
nitishraghunathan <nitish.raghunathan1994@gmail.com>
2025-05-25 15:14:59 -0700

    Time: 95 ms (19.06%) | Memory: 32 MB (66.71%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.py
+++ 53-maximum-subarray/maximum-subarray.py
@@ -1,8 +1,13 @@
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
+        """
+        1. Iterate through the array
+        2. Compute the sum at every index during the iteration.
+        3. find the sum and well as the max_sum at that index
+        """        
         max_sum = float('-inf')
-        current_sum = float('-inf')
-        for num in nums:
-            current_sum = max(current_sum+num, num)
-            max_sum = max(max_sum, current_sum)
-        return max_sum        
+        compute_sum = 0
+        for index, value in enumerate(nums):
+            compute_sum = max(compute_sum + value, value)
+            max_sum = max(max_sum, compute_sum)
+        return max_sum
