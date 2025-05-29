ee3168e1ffba77456df9df73bcb3b57bcac20a4c
nitishraghunathan <nitish.raghunathan1994@gmail.com>
2025-05-29 16:51:15 -0700

    Time: 375 ms (5.01%) | Memory: 19.3 MB (39.87%) - LeetSync

--- 207-course-schedule/course-schedule.py
+++ 207-course-schedule/course-schedule.py
@@ -11,23 +11,30 @@ class Solution:
         8. Once all the queue elements are evaluated if the len of result != numOfCourses then we cannot form a course schedule 
         9. Otherwise we can
         """
-        course_dictionary = {}
-        for i in range(numCourses):
-            if i not in course_dictionary:
-                course_dictionary[i] = set()
-        for req in prerequisites:
-            course_dictionary[req[0]].add(req[1])
+        dependent_courses = {}
         queue = []
-        for key, value in course_dictionary.items():
-            if not value:
+        for i in range(numCourses):
+            if i not in dependent_courses:
+                dependent_courses[i] = set()
+
+        for arr in prerequisites:
+            if arr[0] not in dependent_courses:
+                dependent_courses[arr[0]] = set()
+            dependent_courses[arr[0]].add(arr[1])
+        for key, value in dependent_courses.items():
+            if len(value) == 0:
                 queue.append(key)
-        result = []
+        final_list = []
         while queue:
+            size = len(queue)
             course_id = queue.pop(0)
-            result.append(course_id)
-            for key, value in course_dictionary.items():
+            for key, value in dependent_courses.items():
                 if course_id in value:
                     value.remove(course_id)
-                    if not value:
+                    if len(value) == 0:
                         queue.append(key)
-        return len(result) == numCourses
+            final_list.append(course_id)
+        return numCourses == len(final_list)
+            
+
+
