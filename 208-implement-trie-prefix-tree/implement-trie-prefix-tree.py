f761b87eddbdb04047c09c9635bb464f6e1fc3b7
nitishraghunathan <nitish.raghunathan1994@gmail.com>
2025-05-29 17:18:48 -0700

    Time: 43 ms (72.49%) | Memory: 32.8 MB (24.09%) - LeetSync

--- 208-implement-trie-prefix-tree/implement-trie-prefix-tree.py
+++ 208-implement-trie-prefix-tree/implement-trie-prefix-tree.py
@@ -2,35 +2,40 @@ class Trie:
 
     def __init__(self):
         self.root = TrieNode("/")
-        
-
+    
     def insert(self, word: str) -> None:
         current = self.root
-        for character in word:
-            if character not in current.word_dict:
-                current.word_dict[character] = TrieNode(character)
-            current = current.word_dict[character]
-        current.isWord = True        
-
+        for index,value in enumerate(word):
+            if value not in current.map_dict:
+                current.map_dict[value] = TrieNode(value)
+            current = current.map_dict[value]
+        current.is_word = True
+    
     def search(self, word: str) -> bool:
-        return self.search_helper(word, False)
-        
-    def search_helper(self, word, flag):
         current = self.root
-        for character in word:
-            if character not in current.word_dict:
+        for index,value in enumerate(word):
+            if value not in current.map_dict:
                 return False
-            current = current.word_dict[character]
-        return current.isWord if not flag else True
+            else:
+                current = current.map_dict[value]
+        return current.is_word
 
-    def startsWith(self, prefix: str) -> bool:
-        return self.search_helper(prefix, True)
-
+    def startsWith(self, word: str) -> bool:
+        current = self.root
+        for index,value in enumerate(word):
+            if not current:
+                return False
+            elif value not in current.map_dict:
+                return False
+            else:
+                current = current.map_dict[value]
+        return True
+        
 class TrieNode:
-    def __init__(self, character):
-        self.word_dict = {}
-        self.character = character 
-        self.isWord = False
+    def __init__(self, alphabet):
+        self.char = alphabet
+        self.map_dict = {}
+        self.is_word = False
 
 # Your Trie object will be instantiated and called as such:
 # obj = Trie()
