663ce9564ff4a18bcd1020e5cdc46ff76a653d7a
nitishraghunathan <nitish.raghunathan1994@gmail.com>
2025-05-31 05:18:47 -0700

    Time: 19 ms (87.21%) | Memory: 23.8 MB (38.88%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.py
+++ 238-product-of-array-except-self/product-of-array-except-self.py
@@ -5,14 +5,12 @@ class Solution:
         right_arr = [1]*size
         for i in range(1, size):
             left_arr[i] = left_arr[i-1]*nums[i-1]
+        right = 1
+        for i in range(size-1, -1, -1):
+            left_arr[i] = left_arr[i]*right
+            right*=nums[i]
 
-        for i in range(size-2, -1, -1):
-            right_arr[i] = right_arr[i+1]*nums[i+1]
-        nums[0] = right_arr[0]
-        nums[-1] = left_arr[-1]
-        for i in range(1, size-1):
-            nums[i] = left_arr[i]*right_arr[i]
-        return nums
+        return left_arr
 
 
         
