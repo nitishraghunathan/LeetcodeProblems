f2d78f9551082f58418ce0ca2f53e4d36d9f4398
nitishraghunathan <nitish.raghunathan1994@gmail.com>
2025-06-07 07:18:58 -0700

    Time: 0 ms (100.00%) | Memory: 17.8 MB (53.88%) - LeetSync

--- 75-sort-colors/sort-colors.py
+++ 75-sort-colors/sort-colors.py
@@ -2,41 +2,17 @@ class Solution:
     def sortColors(self, nums: List[int]) -> None:
         """
         Do not return anything, modify nums in-place instead.
-        Using two pointers
-        p0 - 0 
-        p1 = len(nums)-1
-        The algorithm is like follows:
-        [2,0,2,1,1,0]
-        while 0 - push right
-        while 2 - push left
-        while rig
         """
-        def add_numbers(counter, left, number, nums):
-            while counter > 0 and left < len(nums):
-                nums[left] = number
-                counter-=1
-                left +=1
-            return nums, left
-        counter_zero = 0
-        counter_one = 0
-        counter_two = 0
-        for index,value in enumerate(nums):
-            if value == 0:
-                counter_zero +=1
-            elif value == 1:
-                counter_one +=1
+        zero, first = 0, 0
+        second = len(nums) - 1
+        while first <= second:
+            if nums[first] == 0:
+                nums[first], nums[zero] = nums[zero], nums[first]
+                zero +=1
+                first+=1
+            elif nums[first] == 2:
+                nums[first], nums[second] = nums[second], nums[first]
+                second -=1
             else:
-                counter_two +=1
-        left = 0
-        nums, left = add_numbers(counter_zero, left, 0, nums)
-        nums, left = add_numbers(counter_one, left, 1, nums)
-        nums, left = add_numbers(counter_two, left, 2, nums)
+                first+=1
         return nums
-
-
-
-
-
-
-
-        
