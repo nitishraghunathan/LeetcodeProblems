17c6c1790e5dd84389801285e6f6541c4669ed8b
nitishraghunathan <nitish.raghunathan1994@gmail.com>
2025-06-08 14:53:59 -0700

    Time: 6856 ms (7.53%) | Memory: 18.3 MB (57.40%) - LeetSync

--- 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.py
+++ 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.py
@@ -3,7 +3,6 @@ class Solution:
         p = sorted(p)
         result = []
         for i in range(len(s) - len(p)+1):
-            print(i)
             if sorted(s[i:i+len(p)]) == p:
                 result.append(i) 
         return result
