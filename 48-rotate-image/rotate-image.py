b4a134edc1ba18f916bd798276cc89ab6525629f
nitishraghunathan <nitish.raghunathan1994@gmail.com>
2025-06-08 12:30:36 -0700

    Time: 0 ms (100.00%) | Memory: 18 MB (22.91%) - LeetSync

--- 48-rotate-image/rotate-image.py
+++ 48-rotate-image/rotate-image.py
@@ -4,6 +4,8 @@ class Solution:
         Do not return anything, modify matrix in-place instead.
         1,0 -> 0,1 
         1,2 -> 2,1
+        In order to do transpose of a matrix we need to increase the column counter to be 1 greatee then the row counte,r so that we dont do any repeatations 
+        
         """
         for i in range(len(matrix)):
             for j in range(i+1, len(matrix[i])):
