8c9861f5d7717bb7d64e8a66e476efe37241331e
nitishraghunathan <nitish.raghunathan1994@gmail.com>
2025-06-08 07:42:05 -0700

    Time: 120 ms (6.61%) | Memory: 28.3 MB (90.31%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.py
+++ 11-container-with-most-water/container-with-most-water.py
@@ -1,11 +1,20 @@
 class Solution:
     def maxArea(self, height: List[int]) -> int:
-        max_area = float('-inf')
-        left, right = 0, len(height) - 1
-        while left < right:
-            max_area = max(max_area, min(height[left], height[right])*(right-left))
-            if height[left] > height[right]:
-                right -=1
-            else:
+        """
+        We are given height of lines which span from zero at the Ith index to height[i].
+        create a container which can stor maximum water based on area
+        [1,8,6,2,5,4,8,3,7]
+        In order to find the container compute the minimum height and distance between the left most pointer and right most pointer
+        """
+        max_area = float("-inf")
+        min_height = float("inf")
+        left = 0
+        right = len(height) - 1
+        while left <= right:
+            min_height = min(height[left], height[right])
+            max_area = max(max_area, min_height*(abs(left - right)))
+            if height[left] < height[right]:
                 left +=1
+            else:
+                right -=1
         return max_area
