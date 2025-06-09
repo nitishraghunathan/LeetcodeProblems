6e14814eabfa396cf0b7ab393befc3a2be391fbe
nitishraghunathan <nitish.raghunathan1994@gmail.com>
2025-06-09 14:31:49 -0700

    Time: 70 ms (64.01%) | Memory: 18.2 MB (62.17%) - LeetSync

--- 271-encode-and-decode-strings/encode-and-decode-strings.py
+++ 271-encode-and-decode-strings/encode-and-decode-strings.py
@@ -1,22 +1,14 @@
 class Codec:
-    def __init__ (self):
-        self.map_dict = {}
-
     def encode(self, strs: List[str]) -> str:
         """Encodes a list of strings to a single string.
         """
-        hash_code = hash(''.join(strs))
-        self.map_dict[hash_code] = strs
-        return hash_code
-        
-        
+        return "null".join(strs)
 
     def decode(self, s: str) -> List[str]:
         """Decodes a single string to a list of strings.
         """
-        if s in self.map_dict:
-            return self.map_dict[s]
-        return ""
+        return s.split("null")
+        
 
 
 # Your Codec object will be instantiated and called as such:
