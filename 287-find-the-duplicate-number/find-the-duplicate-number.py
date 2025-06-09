d4ba279aa8f173ce2e9d778ee2fc6a21a6ef5390
nitishraghunathan <nitish.raghunathan1994@gmail.com>
2025-06-09 10:35:25 -0700

    Time: 54 ms (18.75%) | Memory: 29.9 MB (78.35%) - LeetSync

--- 287-find-the-duplicate-number/find-the-duplicate-number.py
+++ 287-find-the-duplicate-number/find-the-duplicate-number.py
@@ -1,7 +1,12 @@
 class Solution:
     def findDuplicate(self, nums: List[int]) -> int:
-        nums = sorted(nums)
-        for i in range(len(nums)):
-            if nums[i] == nums[i+1]:
-                return nums[i]
+        """
+        Chose the index of the element - 1 and , if it is already negative it is duplicate
+        """
+        sum_array = sum(nums)
+        for index, value in enumerate(nums):
+            nums[abs(value)-1] = -nums[abs(value)-1]
+            if nums[abs(value)-1] > 0:
+                return abs(value)
         return -1
+        
