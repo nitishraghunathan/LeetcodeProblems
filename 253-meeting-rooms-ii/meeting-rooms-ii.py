e05bafb85d24835e6ba633612ff5c55eecd468ce
nitishraghunathan <nitish.raghunathan1994@gmail.com>
2025-06-09 19:22:46 -0700

    Time: 5 ms (41.81%) | Memory: 20 MB (49.72%) - LeetSync

--- 253-meeting-rooms-ii/meeting-rooms-ii.py
+++ 253-meeting-rooms-ii/meeting-rooms-ii.py
@@ -1,12 +1,5 @@
 class Solution:
     def minMeetingRooms(self, intervals: List[List[int]]) -> int:
-
-        """
-        sort all the rooms based on intervals start time
-        add the first meeting room 
-        1. compare all the intervals start time with the end time of peek element in the queue
-        2. if end time start time is less than end time push it to the queue else pop it off
-        """
         intervals = sorted(intervals, key = lambda x:x[0])
         results = []
         heapq.heappush(results, intervals[0][1])
@@ -15,6 +8,3 @@ class Solution:
                 heapq.heappop(results)
             heapq.heappush(results, intervals[i][1])
         return len(results)
-
-
-            
