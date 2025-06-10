a3bb02884842debba642b049b4a6a679f03ceb17
nitishraghunathan <nitish.raghunathan1994@gmail.com>
2025-06-10 12:17:52 -0700

    Time: 4 ms (80.47%) | Memory: 19.4 MB (5.08%) - LeetSync

--- 32-longest-valid-parentheses/longest-valid-parentheses.py
+++ 32-longest-valid-parentheses/longest-valid-parentheses.py
@@ -1,22 +1,15 @@
 class Solution:
     def longestValidParentheses(self, s: str) -> int:
-        """
-        1. Initialize a stack and keep track of the index of the brackets
-        2. if open then just push it along with index 
-        3. if closed check if last element is open or push it in the stack with index to for counting new subsrtring
-
-        """
         stack = []
-        max_length = 0
-        stack.append(['', -1])
-        for index, value in enumerate(s):
+        max_distance = 0
+        stack.append(["", -1])
+        for index,value in enumerate(s):
             if value == '(':
                 stack.append([value, index])
             else:
-                if stack and stack[-1][0] == '(':
+                if stack and stack[-1][0] == "(":
                     stack.pop()
-                    max_length = max(max_length, index - stack[-1][1])
+                    max_distance = max(max_distance, index-stack[-1][1])
                 else:
                     stack.append([value, index])
-        return max_length
-
+        return max_distance
