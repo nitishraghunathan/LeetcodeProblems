f57dd872a961ff1c1446556a4d56e88424b2b138
nitishraghunathan <nitish.raghunathan1994@gmail.com>
2025-06-10 05:35:38 -0700

    Time: 4 ms (10.91%) | Memory: 17.5 MB (99.86%) - LeetSync

--- 91-decode-ways/decode-ways.py
+++ 91-decode-ways/decode-ways.py
@@ -1,8 +1,14 @@
 class Solution:
     def numDecodings(self, s: str) -> int:
-        map_dict = {}
-        for i in range(65, 91):
-            map_dict[i-64] = chr(i)
+        """
+        Algorithm: check if the first digit is zero
+        if no then add dp[1] = 1
+        dp[0] = 1 always as there will always be 1 way to represent an empty string 
+        dp[1] = 1 if s[i-1]!='0'
+        no starting from 2nd index lets say how many ways can  be formed. 
+        s[i-1] is not zero then there would be atleast dp[i-1] ways 
+        else if s[i-2:i] <26 and >10 then +dp[i-2] ways 
+        """
         dp = [0]*(len(s)+1)
         dp[0] = 1
         dp[1] =0 if s[0] == '0' else 1
@@ -12,4 +18,6 @@ class Solution:
             if int(s[i-2:i]) >= 10 and  int(s[i-2:i]) <=26:
                 dp[i] +=dp[i-2]
         return dp[len(s)]
+
+
 
