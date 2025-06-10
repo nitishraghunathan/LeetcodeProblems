7ab34a7fdcf5d66919a8255ab793df6faca3d6f7
nitishraghunathan <nitish.raghunathan1994@gmail.com>
2025-06-09 19:58:57 -0700

    Time: 0 ms (100.00%) | Memory: 17.9 MB (49.34%) - LeetSync

--- 50-powx-n/powx-n.py
+++ 50-powx-n/powx-n.py
@@ -6,8 +6,6 @@ class Solution:
             n = -n
         map_dict[0] = 1
         def recursion(x: float, n:int):
-            if n == 0:
-                return 1
             if n in map_dict:
                 return map_dict[n]
             compute_x_power = recursion(x, n//2)*recursion(x, n//2)
