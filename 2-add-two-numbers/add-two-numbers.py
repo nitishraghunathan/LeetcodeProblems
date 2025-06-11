3ac190a7f6c401373581c4749ea6782a8ac43502
nitishraghunathan <nitish.raghunathan1994@gmail.com>
2025-06-11 06:28:21 -0700

    Time: 4 ms (55.40%) | Memory: 17.9 MB (56.70%) - LeetSync

--- 2-add-two-numbers/add-two-numbers.py
+++ 2-add-two-numbers/add-two-numbers.py
@@ -5,35 +5,20 @@
 #         self.next = next
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        temp = ListNode(-1)
-        dummy = temp
+        current = ListNode(-1)
+        dummy = current
         carry = 0
-        sum_val = 0
-        while l1 and l2:
+        while l1 or l2 or carry > 0:
             x = l1.val if l1 else 0
-            y = l2.val if l2 else 0        
-            temp.next = ListNode((carry + x +y)%10)
-            temp =temp.next
-            print(temp.val)
-            carry = (x+y+carry)//10
+            y = l2.val if l2 else 0 
             if l1:
-                l1 =l1.next
+                l1 = l1.next
             if l2:
-                l2 =l2.next
-
-        while l1:
-            temp.next =ListNode((l1.val+carry)%10)
-            temp =temp.next
-            carry = (l1.val+carry)//10
-            l1 = l1.next
-        while l2:
-            temp.next =ListNode((l2.val+carry)%10)
-            temp =temp.next
-            carry = (l2.val+carry)//10
-            l2 = l2.next
-        if carry:
-            temp.next= ListNode(carry)
-            temp =temp.next
-        temp = dummy
-        return temp.next
-        
+                l2 = l2.next
+            sum_value = (carry+x+y)%10
+            carry = (carry+x+y)//10
+            new_node = ListNode(sum_value)
+            current.next  = new_node
+            current = current.next
+        current = dummy
+        return current.next
