d28d22cd87d48b71c5367537fb7b6d58ce1fff18
nitishraghunathan <nitish.raghunathan1994@gmail.com>
2025-06-10 18:20:48 -0700

    Time: 519 ms (11.13%) | Memory: 18.2 MB (77.12%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.py
+++ 76-minimum-window-substring/minimum-window-substring.py
@@ -1,32 +1,56 @@
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
-        map_one , map_two= {}, {}
-        for ti in t: 
-            if ti not in map_one:
-                map_one[ti] =0
-            map_one[ti] +=1
-
-        def check_map(map_one:dict, map_two:dict):
-            for key, values in map_one.items():
-                if key not in map_two or values > map_two[key]:
+        """
+        Requirements: 
+        String: s 
+        Substring to match: t
+        Problem: Find minimum number of characters in S to match substring t
+        ALgorithm: Two pointer approach + map 
+        1. Have two maps:
+            a. Store the elements of t in the map
+            b. Store a new map which keeps track of the elements in the substring interation in S.
+            c. Compare the two maps and once they are equal return the minimum substring
+        2. Increase the pointer consistently till we reach the end.
+        3. keep track of size and minimum substring
+        """
+        def check_map(tracking_map: dict, substring_map: dict) -> bool:
+            for key, value in substring_map.items():
+                if key not in tracking_map or value > tracking_map[key]:
                     return False
             return True
-        left, right  = 0, len(s)
-        min_length = float('inf')
-        min_value = ''
-        index = 0
-        while left < right:
-            if s[left] not in map_two:
-                map_two[s[left]] =0
-            map_two[s[left]] +=1     
-            left+=1
-            while check_map(map_one, map_two):
-                if left-index < min_length:
-                    min_value = s[index:left]
-                    min_length = left-index
-                map_two[s[index]] -=1
-                if map_two[s[index]] == 0:
-                    map_two.pop(s[index])
-                index+=1
-        return min_value
 
+        left = 0
+        right = len(s)
+        index = 0
+        min_substring = ""
+        min_size = float('inf')
+        substring_map = {}
+        tracking_map = {}
+        for item in t:
+            if item not in substring_map:
+                substring_map[item] = 0
+            substring_map[item] +=1 
+        front_pointer = 0
+        while left < right:
+            if s[left] not in  tracking_map:
+                tracking_map[s[left]] = 0
+            tracking_map[s[left]] +=1
+            front_pointer = index
+            left += 1
+            while check_map(tracking_map, substring_map):
+                if left - front_pointer <= min_size:
+                    min_size = left - front_pointer
+                    min_substring = s[front_pointer : left]
+                   
+                tracking_map[s[front_pointer]] -= 1
+                if tracking_map[s[front_pointer]] == 0:
+                    tracking_map.pop(s[front_pointer])
+                front_pointer +=1
+                index = front_pointer
+        return min_substring
+                
+
+                
+
+
+    
