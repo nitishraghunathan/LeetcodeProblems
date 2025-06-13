d385236ac5da7cc715a7e9274e2338f2ea1673bc
nitishraghunathan <nitish.raghunathan1994@gmail.com>
2025-06-12 17:44:43 -0700

    Time: 63 ms (86.64%) | Memory: 26.4 MB (62.71%) - LeetSync

--- 931-maximum-frequency-stack/maximum-frequency-stack.py
+++ 931-maximum-frequency-stack/maximum-frequency-stack.py
@@ -2,32 +2,31 @@ class FreqStack:
 
     def __init__(self):
         self.map_dict = {}
-        self.max_count = 0
-        self.group = []
+        self.group_list = []
+        self.max_size = 0
         
 
     def push(self, val: int) -> None:
         if val not in self.map_dict:
             self.map_dict[val] = 0
-        self.map_dict[val] +=1
-        if self.map_dict[val] > self.max_count:
-            self.max_count = self.map_dict[val]
-        if self.max_count > len(self.group):
-            self.group.append([])
-        self.group[self.map_dict[val]-1].append(val)        
+        self.map_dict[val] += 1
+        if self.map_dict[val] > self.max_size:
+            self.group_list.append([])
+            self.max_size = self.map_dict[val]
+        self.group_list[self.map_dict[val]-1].append(val)
 
     def pop(self) -> int:
-       key = -1
-       if self.group:
-            key = self.group[self.max_count-1].pop()
-       self.map_dict[key] -=1
-       if self.map_dict[key] == 0:
-            del self.map_dict[key]
-       if not self.group[self.max_count-1]:
-            self.group.pop()
-            self.max_count-=1
-       return key
-        
+       if not self.group_list:
+            return -1
+       val = self.group_list[self.max_size-1].pop()
+       if not self.group_list[self.max_size-1]:
+          self.group_list.pop()
+          self.max_size -=1
+       self.map_dict[val] -=1
+       if self.map_dict[val] == 0:
+          self.map_dict.pop(val)
+       return val
+
 
 
 # Your FreqStack object will be instantiated and called as such:
