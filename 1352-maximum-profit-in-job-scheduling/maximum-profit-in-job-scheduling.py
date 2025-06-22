719b9abc74183244493d9e00e91a2a303dd10bec
nitishraghunathan <nitish.raghunathan1994@gmail.com>
2025-06-22 09:35:18 -0700

    Time: 445 ms (5.02%) | Memory: 53.8 MB (35.80%) - LeetSync

--- 1352-maximum-profit-in-job-scheduling/maximum-profit-in-job-scheduling.py
+++ 1352-maximum-profit-in-job-scheduling/maximum-profit-in-job-scheduling.py
@@ -1,31 +1,36 @@
 class Solution:
     def jobScheduling(self, startTime: List[int], endTime: List[int], profit: List[int]) -> int:
         """
-        Algorithm: use DP memoization
-        first take all the arrays, zip it and then sort it based on start time 
-        use recursion to choose and not choose interval
-        when we choose the next interval the startime should b greater than the end time of the current interval
+        These are the cocepts we need to be mindful of
+        1. Binary Search
+        2. Dynamic Programming
+        3. Recursion
+        1 - Get a list of lists having start_time, end_time, profit
+        2. Use recursion to interate every job, we should use choice based recursion,
+        we pick the job anfd ctake the next job whose start time is greater than end time
+        else we don't pick and find the next job. we compuet the max profit for both
+        3. Return the max profit
         """
-        intervals = sorted(zip(startTime,endTime, profit))
-        dp = [-1]*len(intervals)
-        def dfs(intervals, dp, index):
-            if len(intervals) == index:
+        intervals = sorted(zip(startTime, endTime, profit))
+        dp = [-1]*len(startTime)
+        def dfs(index):
+            if index == len(startTime):
                 return 0
             if dp[index] != -1:
                 return dp[index]
-            def binary_search(left, right, array, end_time):
-                next_index = len(array)
-                while left<=right:
+            def binary_search(left, right, endTime):
+                index = len(startTime)
+                while left <= right:
                     mid = left + (right-left)//2
-                    if array[mid][0] < end_time:
-                        left=mid+1
+                    if intervals[mid][0] < endTime:
+                        left = mid +1
                     else:
-                        right = mid-1
-                        next_index=mid
-                return next_index
-            new_index = binary_search(0, len(intervals)-1, intervals, intervals[index][1])
-            result = max(dfs(intervals,dp, index+1), intervals[index][2]+ dfs(intervals, dp, new_index))
+                        right = mid -1
+                        index = mid 
+                return index
+            new_index = binary_search(index, len(startTime)-1, intervals[index][1])
+            result = max(dfs(index+1), intervals[index][2] + dfs(new_index))
             dp[index] = result
             return dp[index]
-        return dfs(intervals,dp,0)                    
+        return dfs(0)
         
