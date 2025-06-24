c6576e5271174b6c314a3e5835ced63201b49a06
nitishraghunathan <nitish.raghunathan1994@gmail.com>
2025-06-24 02:31:49 -0700

    Time: 71 ms (60.88%) | Memory: 23.3 MB (86.88%) - LeetSync

--- 912-random-pick-with-weight/random-pick-with-weight.py
+++ 912-random-pick-with-weight/random-pick-with-weight.py
@@ -1,24 +1,24 @@
 class Solution:
 
     def __init__(self, w: List[int]):
-        self.prefix_total_sum = []
-        self.prefix_sum = 0
+        self.total_sum = 0
+        self.prefix_sum_list = []
         for weights in w:
-            self.prefix_sum += weights
-            self.prefix_total_sum.append(self.prefix_sum)
-
+            self.total_sum += weights
+            self.prefix_sum_list.append(self.total_sum)
+        
 
     def pickIndex(self) -> int:
-
-        target = self.prefix_sum*random.random()
-        low, high = 0, len(self.prefix_total_sum)
-        while low < high:
-            mid = low + (high-low)//2
-            if self.prefix_total_sum[mid] < target:
-                low = mid+1
+        target = self.total_sum*random.random()
+        left, right = 0, len(self.prefix_sum_list)-1
+        while left < right:
+            mid = left + (right-left)//2
+            if self.prefix_sum_list[mid] < target:
+                left = mid +1
             else:
-                high = mid
-        return low
+                right =mid
+        return left
+
         
 
 
