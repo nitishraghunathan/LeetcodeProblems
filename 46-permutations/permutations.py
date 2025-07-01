ce311e79b8c9a3b62085ca5d1355b3fc493dac56
nitishraghunathan <nitish.raghunathan1994@gmail.com>
2025-07-01 05:18:51 -0700

    Time: 1 ms (47.93%) | Memory: 18.1 MB (33.39%) - LeetSync

--- 46-permutations/permutations.py
+++ 46-permutations/permutations.py
@@ -1,6 +1,6 @@
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
-        result = set()
+        result = []
         """
         1. permutations to this problem
         recursion, pass in a parameter
@@ -8,12 +8,12 @@ class Solution:
         """
         def define_permutations(nums, index):
             if index == len(nums):
-                result.add(tuple(nums))
+                result.append(list(nums))
                 return result
-            for i in range(len(nums)):
+            for i in range(index, len(nums)):
                 nums[i], nums[index] = nums[index], nums[i]
                 define_permutations(nums, index+1)
                 nums[i], nums[index] = nums[index], nums[i]
             return result
         define_permutations(nums, 0)
-        return list(result)
+        return result
