bc4c31022559355ad54ed8a06ac189a2b19e330e
nitishraghunathan <nitish.raghunathan1994@gmail.com>
2025-07-16 15:48:20 -0700

    Time: 0 ms (100.00%) | Memory: 17.9 MB (62.96%) - LeetSync

--- 362-design-hit-counter/design-hit-counter.py
+++ 362-design-hit-counter/design-hit-counter.py
@@ -2,21 +2,21 @@ class HitCounter:
 
     def __init__(self):
         self.queue = []
-        
 
     def hit(self, timestamp: int) -> None:
         self.queue.append(timestamp)
         
 
     def getHits(self, timestamp: int) -> int:
-        lower_limit  = timestamp-300
-        counter = 0
-        for i in range(len(self.queue)):
-            if self.queue[i] > timestamp:
-                return counter
-            elif lower_limit < self.queue[i] <= timestamp:
-                counter+=1
-        return counter
+        lower_limit = timestamp-300
+        left, right = 0, len(self.queue)-1
+        while left <= right:
+            mid = left + (right-left)//2
+            if self.queue[mid] <= lower_limit:
+                left = mid+1
+            else:
+                right = mid-1
+        return len(self.queue)-left
         
 
 
