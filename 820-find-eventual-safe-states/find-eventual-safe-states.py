5e5fa320dd4363b019b63a2ee2a5fb8af330dd52
nitishraghunathan <nitish.raghunathan1994@gmail.com>
2025-11-15 13:11:03 -0800

    Time: 51 ms (48.68%) | Memory: 23.8 MB (37.32%) - LeetSync

--- 820-find-eventual-safe-states/find-eventual-safe-states.py
+++ 820-find-eventual-safe-states/find-eventual-safe-states.py
@@ -1,22 +1,35 @@
 class Solution:
     def eventualSafeNodes(self, graph: List[List[int]]) -> List[int]:
-        n=len(graph)
-        safe={}
-        ans=[]
-        def dfs(i):
-            if i in safe:
-                return safe[i]
-
-            safe[i]=False
-            for nei in graph[i]:
-                if not dfs(nei):
-                    return safe[i]
-
-            safe[i]=True
-            return safe[i]
+        n = len(graph)
+        indegree = [0] * n
+        adj = [[] for _ in range(n)]
 
         for i in range(n):
-            if dfs(i):
-                ans.append(i)
+            for node in graph[i]:
+                adj[node].append(i)
+                indegree[i] += 1
 
-        return ans                        
+        q = deque()
+        # Push all the nodes with indegree zero in the queue.
+        for i in range(n):
+            if indegree[i] == 0:
+                q.append(i)
+
+        safe = [False] * n
+        while q:
+            node = q.popleft()
+            safe[node] = True
+
+            for neighbor in adj[node]:
+                # Delete the edge "node -> neighbor".
+                indegree[neighbor] -= 1
+                if indegree[neighbor] == 0:
+                    q.append(neighbor)
+
+        safeNodes = []
+        for i in range(n):
+            if safe[i]:
+                safeNodes.append(i)
+
+        return safeNodes
+        
