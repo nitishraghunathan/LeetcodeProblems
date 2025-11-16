2ffb0baecd1a7020bc0ca81600e3e76a468cce71
nitishraghunathan <nitish.raghunathan1994@gmail.com>
2025-11-15 17:40:20 -0800

    Time: 3 ms (25.18%) | Memory: 18.3 MB (29.55%) - LeetSync

--- 871-keys-and-rooms/keys-and-rooms.py
+++ 871-keys-and-rooms/keys-and-rooms.py
@@ -1,12 +1,14 @@
 class Solution:
     def canVisitAllRooms(self, rooms: List[List[int]]) -> bool:
-        def dfs(rooms:list, src:int, visited: set):
-            visited.add(src)
-            if len(visited) == len(rooms):
-                return True
-            flag = False
-            for dest in rooms[src]:
-                if dest not in visited:
-                    flag |= dfs(rooms, dest, visited)
-            return flag
-        return dfs(rooms,0, set())
+        queue = []
+        queue.append(0)
+        visited = set()
+        while queue:
+            key = queue.pop(0)
+            visited.add(key)
+            for index, value in enumerate(rooms[key]):
+                if value in visited:
+                    continue
+                queue.append(value)
+        return len(rooms) == len(visited)
+        
