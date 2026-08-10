3fc8655d328d89af1980179c47fa1ed5e6b3a984
nitishraghunathan <nitish.raghunathan1994@gmail.com>
2026-08-10 06:27:20 -0400

    Time: 15 ms (5.25%) | Memory: 19.2 MB (88.88%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.py
+++ 13-roman-to-integer/roman-to-integer.py
@@ -1,24 +1,19 @@
 class Solution:
-    def romanToInt(self, nums: str) -> int:
-        map_dict = {
-            'I': 1,
-            'V': 5,
-            'X': 10,
-            'L': 50,
-            'C': 100,
-            'D': 500,
-            'M': 1000,
+    def romanToInt(self, s: str) -> int:
+        roman_map = {
+            'I': 1, 'V': 5, 'X': 10, 'L': 50,
+            'C': 100, 'D': 500, 'M': 1000
         }
         total = 0
-        for i in range(len(nums)):
-            if i < len(nums)-1 and ((nums[i] == 'I' \
-            and nums[i+1] in {'V', 'X'}) \
-            or (nums[i] == 'X' and nums[i+1] in {'L', 'C'}) \
-            or (nums[i] == 'C' and nums[i+1] in {'D', 'M'})):
-                total -= map_dict[nums[i]]
+        max_seen = 0
+        
+        # Loop backwards through the string
+        for char in reversed(s):
+            val = roman_map[char]
+            if val < max_seen:
+                total -= val
             else:
-                total += map_dict[nums[i]]
+                total += val
+                max_seen = val
+                
         return total
-            
-
-                    
