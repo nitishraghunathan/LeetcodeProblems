a3b7fb063a5b35934f0f745de413046ce555ba06
nitishraghunathan <nitish.raghunathan1994@gmail.com>
2026-08-10 06:43:44 -0400

    Time: 1 ms (44.86%) | Memory: 19.2 MB (91.04%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.py
+++ 20-valid-parentheses/valid-parentheses.py
@@ -1,15 +1,17 @@
 class Solution:
     def isValid(self, s: str) -> bool:
-        map_dict = {')' : '(', '}' : '{', ']' : '['}
+        map_dict = {")" : "(", "]" : "[", "}" : "{"}
         stack = []
         for index, value in enumerate(s):
-            if value in map_dict and stack:
-                diff = stack.pop()
-                if map_dict[value] != diff:
-                    return False
-            else:
+            if value not in map_dict:
                 stack.append(value)
+            else:
+                if stack:
+                    val = stack.pop()
+                    if val != map_dict[value]:
+                        return False
+                else:
+                    return False
         return not stack
-
 
         
