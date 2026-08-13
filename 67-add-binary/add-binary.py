434bbfed87cf8d3e02f4bbdf6b2a879c8c538db7
nitishraghunathan <nitish.raghunathan1994@gmail.com>
2026-08-13 06:57:50 -0400

    Time: 12 ms (4.83%) | Memory: 19.3 MB (84.70%) - LeetSync

--- 67-add-binary/add-binary.py
+++ 67-add-binary/add-binary.py
@@ -1,25 +1,25 @@
 class Solution:
     def addBinary(self, a: str, b: str) -> str:
-        def convert_to_integer(num:str):
-            new_num = 0
-            for i in range(len(num)-1, -1, -1):
-                new_num += pow(2, len(num)-1-i)*int(num[i])
-            return new_num
-        def convert_to_binary_str(num:int):
-            binary_str = ''
-            if num == 0:
-                return '0'
-            while num!=0:
-                binary_str = str(num%2) + binary_str
-                num = num//2
-            return binary_str
-        first_operator = convert_to_integer(a)
-        second_operator = convert_to_integer(b)
-        operator_sum = first_operator + second_operator
-        final_binary_str = convert_to_binary_str(operator_sum)
-        return final_binary_str
+        b_len, a_len = len(b), len(a)
+        if b_len > a_len:
+            return self.addBinary(b,a)
+        diff = a_len - b_len
+        carry, new_str = 0, ""
+        for i in range(a_len-1, -1, -1):
+            if i-diff > -1:
+                sum_val = int(a[i]) + int(b[i-diff]) + carry
+            else:
+                sum_val = int(a[i]) + carry
+
+            carry = sum_val//2
+            total_sum = sum_val%2
+            new_str = str(total_sum) + new_str
+            print(f"This is carry: {carry}")
+            print(f"This is sum: {total_sum}")
+        if carry > 0:
+            new_str = str(carry) + new_str
+        return new_str
+
+           
 
 
-
-
-        
