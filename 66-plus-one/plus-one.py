6ff75534364ad9c9c5b39aca61cd7ca1f8906d69
nitishraghunathan <nitish.raghunathan1994@gmail.com>
2026-08-13 06:19:30 -0400

    Time: 0 ms (100.00%) | Memory: 19.2 MB (55.59%) - LeetSync

--- 66-plus-one/plus-one.py
+++ 66-plus-one/plus-one.py
@@ -1,16 +1,12 @@
 class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
-        if not digits:
-            return 0
         carry = 1
-        for i in range(len(digits)-1, -1, -1):
-            if carry == 0:
-                return digits
-            else:
-                total_sum = (carry+digits[i])%10
-                carry = (carry+digits[i])//10
-                digits[i] = total_sum
-        if carry:
-            digits.insert(0,carry)
+        for i in range(len(digits)-1,-1,-1):
+            sum_val = digits[i]+carry
+            digits[i] = sum_val%10
+            carry = sum_val//10
+        if carry > 0:
+            digits.insert(0, carry)
         return digits
+            
         
