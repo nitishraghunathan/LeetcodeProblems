5f2ab2bb929dd515c51effff84a0d25df445e554
nitishraghunathan <nitish.raghunathan1994@gmail.com>
2026-08-15 08:40:03 -0400

    Time: 0 ms (100.00%) | Memory: 19.5 MB (9.07%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.py
+++ 88-merge-sorted-array/merge-sorted-array.py
@@ -2,10 +2,6 @@ class Solution:
     def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
         """
         Do not return anything, modify nums1 in-place instead.
-        1. Incorporate nums2 into nums1
-        2. sort these lements in the array
-        3. merge and return the array in place
-        4. use two pointer and compare each element in nums1 with nums2 and place the smaller element in nums 1 till we end the zeros
         """
         i = m-1
         j = n-1
@@ -20,4 +16,8 @@ class Solution:
                 k-=1
                 j-=1
         return
+
+
+        
+
         
