e7a62213a273c1e7bb5161da529f8f8b1be4c688
nitishraghunathan <nitish.raghunathan1994@gmail.com>
2026-08-23 08:53:15 -0400

    Time: 943 ms (42.07%) | Memory: 0B (100.00%) - LeetSync

--- 196-delete-duplicate-emails/delete-duplicate-emails.sql
+++ 196-delete-duplicate-emails/delete-duplicate-emails.sql
@@ -1,2 +1,6 @@
--- Write your PostgreSQL query statement below
-DELETE FROM Person p1 USING Person P2 WHERE p1.email=p2.email AND p1.id > p2.id
+# Write your MySQL query statement below
+DELETE p1
+FROM Person p1
+JOIN Person p2 
+  ON p1.email = p2.email 
+ AND p1.id > p2.id;
