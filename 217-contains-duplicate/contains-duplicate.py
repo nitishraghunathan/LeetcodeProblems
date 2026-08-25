cd34ffa2f770b5975de8d15ae4b1c4f8fe7c938e
nitishraghunathan <nitish.raghunathan1994@gmail.com>
2026-08-25 18:39:00 -0400

    Time: 11 ms (82.27%) | Memory: 32.3 MB (33.70%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.py
+++ 217-contains-duplicate/contains-duplicate.py
@@ -1,11 +1,9 @@
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
-        map_dict = {}
-        counter = 0
-        for index,value in enumerate(nums):
-            if value not in map_dict:
-                map_dict[value] = 1
-            else:
+        tracker = set()
+        for num in nums:
+            if num in tracker:
                 return True
+            tracker.add(num)
         return False
         
