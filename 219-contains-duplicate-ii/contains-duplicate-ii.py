98d59cb9f0c84c0d8e158f83abaf8e5f651e4272
nitishraghunathan <nitish.raghunathan1994@gmail.com>
2026-08-25 18:45:51 -0400

    Time: 175 ms (5.00%) | Memory: 48.6 MB (5.00%) - LeetSync

--- 219-contains-duplicate-ii/contains-duplicate-ii.py
+++ 219-contains-duplicate-ii/contains-duplicate-ii.py
@@ -1,13 +1,14 @@
 class Solution:
     def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
         map_dict = {}
-        for i in range(len(nums)):
-            if nums[i] not in map_dict:
-                map_dict[nums[i]] = []
-                map_dict[nums[i]].append(i)
-            else:
-                for value in map_dict[nums[i]]:
-                    if i - value <=k:
-                        return True
-                map_dict[nums[i]].append(i)
+        for index, value in enumerate(nums):
+            if value not in map_dict:
+                map_dict[value] = []
+            map_dict[value].append(index)
+            new_list = map_dict[value]
+            for i in range(1, len(map_dict[value])):
+                if abs(new_list[i] - new_list[i-1]) <= k:
+                    return True
         return False
+
+        
