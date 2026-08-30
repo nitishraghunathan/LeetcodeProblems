ea131162fb8de11695f7344e4c46ddd1ff34e754
nitishraghunathan <nitish.raghunathan1994@gmail.com>
2026-08-30 09:27:29 -0400

    Time: 0 ms (100.00%) | Memory: 19.5 MB (7.32%) - LeetSync

--- 263-ugly-number/ugly-number.py
+++ 263-ugly-number/ugly-number.py
@@ -1,18 +1,9 @@
 class Solution:
     def isUgly(self, n: int) -> bool:
-        # A non-positive integer cannot be ugly
         if n <= 0:
             return False
-
-        # Keep dividing dividend by divisor when division is possible
-        def keep_dividing_when_divisible(dividend, divisor):
-            while dividend % divisor == 0:
-                dividend //= divisor
-            return dividend
-
-        # Factorize by dividing with permitted factors
-        for factor in [2, 3, 5]:
-            n = keep_dividing_when_divisible(n, factor)
-
-        # Check if the integer is reduced to 1 or not.
-        return n == 1
+        prime_factors = [2, 3, 5]
+        for factors in prime_factors:
+            while n%factors==0:
+                n //= factors
+        return n==1
