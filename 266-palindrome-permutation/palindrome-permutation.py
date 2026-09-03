75fcf1a32cdbcfc118577b503db020e82a4da89b
nitishraghunathan <nitish.raghunathan1994@gmail.com>
2026-09-03 06:48:37 -0400

    Time: 0 ms (100.00%) | Memory: 19.3 MB (66.32%) - LeetSync

--- 266-palindrome-permutation/palindrome-permutation.py
+++ 266-palindrome-permutation/palindrome-permutation.py
@@ -1,17 +1,21 @@
 class Solution:
     def canPermutePalindrome(self, s: str) -> bool:
+        odd_count = 0
         map_dict = {}
-        for val in s:
-            if val not in map_dict:
-                map_dict[val] = 1
-            else:
-                map_dict[val] +=1
-        counter = 0
-        for value in map_dict.values():
-            if value%2==1:
-                if counter == 0:
-                    counter +=1
-                else:
-                    return False
-        return True
+        for index, value in enumerate(s):
+            if value not in map_dict:
+                map_dict[value] = 0
+            map_dict[value] += 1
+        for key, value in map_dict.items():
+            if value%2 == 1:
+                odd_count +=1
+        return odd_count < 2 
+
+
+        """
+        Malayalam
+        deed 
+        carerac
+        """
+        
         
