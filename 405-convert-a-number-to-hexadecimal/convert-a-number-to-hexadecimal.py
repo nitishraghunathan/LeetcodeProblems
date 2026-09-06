557eeffc2edc3fd88f11c13297920a4c9a3d323c
nitishraghunathan <nitish.raghunathan1994@gmail.com>
2026-09-06 10:05:31 -0400

    Time: 0 ms (100.00%) | Memory: 19.3 MB (24.91%) - LeetSync

--- 405-convert-a-number-to-hexadecimal/convert-a-number-to-hexadecimal.py
+++ 405-convert-a-number-to-hexadecimal/convert-a-number-to-hexadecimal.py
@@ -2,17 +2,15 @@ class Solution:
     def toHex(self, num: int) -> str:
         if num == 0:
             return "0"
-        
-        # Convert negative integer to its 32-bit unsigned equivalent
+        hex_map = {10:'a', 11: 'b', 12: 'c', 13: 'd', 14: 'e', 15: 'f'}
+        string = ""
         if num < 0:
             num &= 0xFFFFFFFF
-            
-        hex_chars = "0123456789abcdef"
-        result = []
-        
         while num > 0:
-            remainder = num % 16
-            result.append(hex_chars[remainder])
-            num //= 16
-            
-        return "".join(reversed(result))
+            remainder = num%16
+            num = num//16
+            if remainder in hex_map:
+                string = hex_map[remainder] + string
+            else:
+                string = str(remainder) + string 
+        return string
