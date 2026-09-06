3789d76a14ae395a269a37392a0481467ad1de62
nitishraghunathan <nitish.raghunathan1994@gmail.com>
2026-09-05 20:06:28 -0400

    Time: 63 ms (12.70%) | Memory: 20.1 MB (70.00%) - LeetSync

--- 338-counting-bits/counting-bits.py
+++ 338-counting-bits/counting-bits.py
@@ -1,6 +1,10 @@
 class Solution:
     def countBits(self, n: int) -> List[int]:
-        ans = [0] * (n + 1)
-        for x in range(1, n + 1):
-            ans[x] = ans[x & (x - 1)] + 1
-        return ans 
+        def one_bit_counter(n):
+            counter = 0
+            while n > 0:
+                if n%2 == 1:
+                    counter+=1
+                n = n//2
+            return counter
+        return [one_bit_counter(i) for i in range(n+1)]
