f04f9833383103df7b464d3aa28cfc21d100bad1
nitishraghunathan <nitish.raghunathan1994@gmail.com>
2026-09-06 17:28:04 -0400

    Time: 3 ms (46.50%) | Memory: 19.2 MB (92.47%) - LeetSync

--- 409-longest-palindrome/longest-palindrome.py
+++ 409-longest-palindrome/longest-palindrome.py
@@ -1,36 +1,17 @@
 class Solution:
     def longestPalindrome(self, s: str) -> int:
-        """
-        Given a list of lowercase letters and uppercase letters, return the length of the longest palindrome that cna be built with those letters
-        Aa
-        AaA
-        abccccdd
-        What are the rules of palindrome:
-        1. if length is even they should have same 6
-        aabbcc
-        we have a map of lowercase and upper case letters
-        the longest length that can be formed
-        abbba
-        1. Get the count of alphabets in the string
-        2. Determine those alphabets with an even count add them all
-        3. Take any of the with max odd count and then return the answer
-
-        """
         map_dict = {}
-        for index,value in enumerate(s):
+        for index, value in enumerate(s):
             if value not in map_dict:
                 map_dict[value] = 0
-            map_dict[value] += 1
-        even_count_alphabets=0
-        max_odd_count = 0
-        odd_flag = False
-        odd_count = 0
+            map_dict[value] +=1
+        odd_flag, odd_count, even_count = False, 0,0
         for key, value in map_dict.items():
             if value%2==0:
-                even_count_alphabets +=value
+                even_count +=value
             else:
-               odd_flag = True
-               odd_count += value-1
-        return even_count_alphabets + odd_count +1 if odd_flag else even_count_alphabets
-
+                odd_flag = True
+                odd_count += value-1
+        return even_count if not odd_flag else odd_count+even_count+1
+        
         
