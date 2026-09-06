12456433e533f3386d1728efa9bec4740be6c956
nitishraghunathan <nitish.raghunathan1994@gmail.com>
2026-09-05 21:36:54 -0400

    Time: 11 ms (39.22%) | Memory: 22.3 MB (23.31%) - LeetSync

--- 346-moving-average-from-data-stream/moving-average-from-data-stream.py
+++ 346-moving-average-from-data-stream/moving-average-from-data-stream.py
@@ -1,17 +1,19 @@
 class MovingAverage:
 
     def __init__(self, size: int):
-        self.queue = []
         self.size = size
-        self.total_val = 0
+        self.total_sum = 0
+        self.queue = []
+        
 
     def next(self, val: int) -> float:
-        if len(self.queue)== self.size:
-            returned = self.queue.pop(0)
-            self.total_val -= returned
+        if len(self.queue) == self.size:
+            value = self.queue.pop(0)
+            self.total_sum -= value
+        self.total_sum += val
         self.queue.append(val)
-        self.total_val += val
-        return self.total_val/len(self.queue)
+        return self.total_sum/len(self.queue)
+        
 
 
 # Your MovingAverage object will be instantiated and called as such:
