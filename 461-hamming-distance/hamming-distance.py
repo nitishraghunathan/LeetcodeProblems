9f3af711c8e81fea0f596e08b670b145f2eb2874
nitishraghunathan <nitish.raghunathan1994@gmail.com>
2026-09-08 07:13:06 -0400

    Time: 0 ms (100.00%) | Memory: 19.1 MB (98.96%) - LeetSync

--- 461-hamming-distance/hamming-distance.py
+++ 461-hamming-distance/hamming-distance.py
@@ -1,26 +1,26 @@
 class Solution:
     def hammingDistance(self, x, y):
-        def binary(number:int):
+        def binary(number: int):
             result = ""
             while number > 0:
-                if number%2 == 0:
+                if number % 2 == 0:
                     result = "0" + result 
                 else:
                     result = "1" + result 
                 number = number//2
             return result
+
         def append_num(number, diff):
             for i in range(diff):
                 number = "0" + number
             return number
-        first, second = binary(x), binary(y)
-        diff = abs(len(first)- len(second))
 
+        first, second = binary(x), binary(y)
+        diff = abs(len(first) - len(second))
         if len(first) > len(second):
             second = append_num(second, diff)
         elif len(second) > len(first):
             first = append_num(first, diff)
-        print(f"{first}:{second}")
         counter = 0
         for i in range(len(first)):
             if first[i] != second[i]:
