69a43e109b3ea0bcd2e412355d36300855eefb59
nitishraghunathan <nitish.raghunathan1994@gmail.com>
2026-09-16 06:35:42 -0400

    Time: 0 ms (100.00%) | Memory: 19.1 MB (99.13%) - LeetSync

---
+++ 500-keyboard-row/keyboard-row.py
@@ -0,0 +1,32 @@
+class Solution:
+    def findWords(self, words: list[str]) -> list[str]:
+        set_one = {"q","w", "e","r","t","y","u", "i", "o", "p"}
+        set_two = {"a","s", "d", "f", "g", "h","j", "k","l"}
+        set_three = {"z","x","c","v","b","n","m"}
+        result = []
+        for word in words:
+            flag_one, flag_two, flag_three = False, False, False
+            break_flag = False
+            for i in range(0, len(word)):
+                if word[i].lower() in set_one:
+                    if flag_two or flag_three:
+                        break_flag = True
+                        break
+                    flag_one = True
+                elif word[i].lower() in set_two:
+                    if flag_three or flag_one:
+                        break_flag = True
+                        break
+                    flag_two = True
+                elif word[i].lower() in set_three:
+                    if flag_one or flag_two:
+                        break_flag = True
+                        break
+                    flag_three = True
+            print(f"The word {word} has the flags flag_one: {flag_one}, flag_two: {flag_two}, flag_three: {flag_three}, break_flag: {break_flag}")
+            if not break_flag:
+                result.append(word)
+        return result
+
+
+        
